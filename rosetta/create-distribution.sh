2c84b3f03b589bfe628dc454ac79779a6b53e3f1
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2023-05-15 14:58:54 -0700

    Standardize variable substitution format

--- rosetta/create-distribution.sh
+++ rosetta/create-distribution.sh
@@ -66,18 +66,18 @@ INSTALLED_DIR=${INSTALLED_DIR:-/opt/t5x}
 DISTRIBUTION_BASE_REF=${DISTRIBUTION_BASE_REF:-main}
 UPSTREAM_GIT_URL=${UPSTREAM_GIT_URL:-https://github.com/google-research/t5x.git}
 
-if [[ -z "$INSTALLED_DIR" ]]; then
+if [[ -z "${INSTALLED_DIR}" ]]; then
   echo "[ERROR]: Need to specify -d/--dir"
   usage 1
 fi
 
-cd $INSTALLED_DIR
+cd ${INSTALLED_DIR}
 
-echo "[INFO]: Basing distribution on commit: $DISTRIBUTION_BASE_REF"
+echo "[INFO]: Basing distribution on commit: ${DISTRIBUTION_BASE_REF}"
 # Switch to main so other branches can be forced updated
 git switch main
 # Create a local branch to mark the base commit
-git branch --force distribution-base $DISTRIBUTION_BASE_REF
+git branch --force distribution-base ${DISTRIBUTION_BASE_REF}
 # Create a local branch for the distribution that starts from the base
 git branch --force rosetta-distribution distribution-base
 git switch rosetta-distribution
@@ -87,62 +87,62 @@ git switch rosetta-distribution
 ####################################
 # Create local branches for all remote branches since remotes aren't recognized
 # when adding local repos as remotes. Excludes origin/HEAD and origin/test_\d+
-if [[ -n "${EXTRA_MIRROR_DIR+x}" ]] && [[ -d $EXTRA_MIRROR_DIR ]]; then
+if [[ -n "${EXTRA_MIRROR_DIR+x}" ]] && [[ -d ${EXTRA_MIRROR_DIR} ]]; then
   EXTRA_REMOTE_NAME=extra
   function git-mirror {
-    git -C $EXTRA_MIRROR_DIR "$@"
+    git -C ${EXTRA_MIRROR_DIR} "$@"
   }
   for remote_branch in $(git-mirror branch --remotes | grep -v 'origin/HEAD' | egrep -v 'origin/test_[0-9]+'); do
     # Try creating a local tracking branch, but if already exists, then update it to match remote.
     # appending -tmp-rosetta in case there's already a local tracking branch with that name.
-    git-mirror branch --track --force $(sed 's/origin\///' <<<$remote_branch)-tmp-rosetta $remote_branch
+    git-mirror branch --track --force $(sed 's/origin\///' <<<${remote_branch})-tmp-rosetta ${remote_branch}
   done
   
-  if git remote show $EXTRA_REMOTE_NAME &>/dev/null; then
-    git remote remove $EXTRA_REMOTE_NAME
+  if git remote show ${EXTRA_REMOTE_NAME} &>/dev/null; then
+    git remote remove ${EXTRA_REMOTE_NAME}
   fi
-  git remote add -f $EXTRA_REMOTE_NAME $EXTRA_MIRROR_DIR
+  git remote add -f ${EXTRA_REMOTE_NAME} ${EXTRA_MIRROR_DIR}
 fi
 
 #################
 # Apply patches #
 #################
 UPSTREAM_REMOTE_NAME=upstream
-if git remote show $UPSTREAM_REMOTE_NAME &>/dev/null; then
-  git remote remove $UPSTREAM_REMOTE_NAME
+if git remote show ${UPSTREAM_REMOTE_NAME} &>/dev/null; then
+  git remote remove ${UPSTREAM_REMOTE_NAME}
 fi
-git remote add -f $UPSTREAM_REMOTE_NAME $UPSTREAM_GIT_URL
+git remote add -f ${UPSTREAM_REMOTE_NAME} ${UPSTREAM_GIT_URL}
 IFS=$'\n'
-for line in $(cat $PATCH_LIST); do
-  if [[ "$line" =~ ^[[:blank:]]*$ ]] || [[ "$line" =~ ^[[:blank:]]*\# ]]; then
+for line in $(cat ${PATCH_LIST}); do
+  if [[ "${line}" =~ ^[[:blank:]]*$ ]] || [[ "${line}" =~ ^[[:blank:]]*\# ]]; then
     continue
   fi
   git_ref=$(awk '{print $1}' <<< "${line}")
-  if [[ "$git_ref" =~ ^pull/ ]]; then
-    REMOTE_NAME=$UPSTREAM_REMOTE_NAME
-    PR_ID=$(cut -d/ -f2 <<<"$git_ref")
+  if [[ "${git_ref}" =~ ^pull/ ]]; then
+    REMOTE_NAME=${UPSTREAM_REMOTE_NAME}
+    PR_ID=$(cut -d/ -f2 <<<"${git_ref}")
     branch=PR-${PR_ID}
-    git fetch $REMOTE_NAME $git_ref:$branch
+    git fetch ${REMOTE_NAME} ${git_ref}:${branch}
   else
-    if [[ -n "${EXTRA_MIRROR_DIR+x}" ]] && [[ -d $EXTRA_MIRROR_DIR ]]; then
-      echo "[WARNING]: EXTRA_MIRROR_DIR=$EXTRA_MIRROR_DIR does not exist so cannot cherry-pick $git_ref"
+    if [[ -n "${EXTRA_MIRROR_DIR+x}" ]] && [[ -d ${EXTRA_MIRROR_DIR} ]]; then
+      echo "[WARNING]: EXTRA_MIRROR_DIR=${EXTRA_MIRROR_DIR} does not exist so cannot cherry-pick ${git_ref}"
       continue
     fi
-    REMOTE_NAME=$EXTRA_REMOTE_NAME
+    REMOTE_NAME=${EXTRA_REMOTE_NAME}
     # Fetch both the feature branch and main so that we can cherry pick the entire branch
-    branch=$REMOTE_NAME/${git_ref}-tmp-rosetta
+    branch=${REMOTE_NAME}/${git_ref}-tmp-rosetta
   fi
-  fork_point=$(git merge-base $REMOTE_NAME/main $branch)
+  fork_point=$(git merge-base ${REMOTE_NAME}/main ${branch})
   ret_code=0
-  git cherry-pick ${fork_point}..$branch || ret_code=$?
-  if [[ $ret_code -ne 0 ]]; then
+  git cherry-pick ${fork_point}..${branch} || ret_code=$?
+  if [[ ${ret_code} -ne 0 ]]; then
     cat <<EOF
-[ERROR]: Tried patching commits from $fork_point..$branch errored. Some possibilities include:
+[ERROR]: Tried patching commits from ${fork_point}..${branch} errored. Some possibilities include:
 
     1. Merge conflict encountered; need to resolve.
-    2. It's possible the branch=$branch wasn't fetched, doesn't exist, or was deleted.
+    2. It's possible the branch=${branch} wasn't fetched, doesn't exist, or was deleted.
 
-Note: $fork_point=\$(git merge-base $REMOTE_NAME/main $branch)
+Note: ${fork_point}=\$(git merge-base ${REMOTE_NAME}/main ${branch})
 
 ==== git status ====
 $(git status)
@@ -155,9 +155,9 @@ EOF
 done
 
 # Cleanup
-for remote in $UPSTREAM_REMOTE_NAME ${EXTRA_REMOTE_NAME:-}; do
-  if git remote show $remote &>/dev/null; then
-    git remote remove $remote
+for remote in ${UPSTREAM_REMOTE_NAME} ${EXTRA_REMOTE_NAME}; do
+  if git remote show ${remote} &>/dev/null; then
+    git remote remove ${remote}
   fi
 done
 if [[ -n "${EXTRA_REMOTE_NAME+x}" ]]; then
