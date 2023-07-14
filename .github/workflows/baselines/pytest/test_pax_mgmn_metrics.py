fe37b046ee1935a8b09d16f5a229b87012e910cf
Maanu Grover <maanug@nvidia.com>
2023-07-14 15:08:38 -0500

    change delta to multiplier, fix conditions

--- .github/workflows/baselines/pytest/test_pax_mgmn_metrics.py
+++ .github/workflows/baselines/pytest/test_pax_mgmn_metrics.py
@@ -5,8 +5,22 @@ import glob
 import test_utils
 from statistics import mean
 
-STEP_TIME_DELTA = 1.0
-E2E_TIME_DELTA = 30.0
+STEP_TIME_MULT = {
+    "1DP1TP1PP":  0.95,
+    "8DP1TP1PP":  0.95,
+    "1DP8TP1PP":  0.95,
+    "2DP1TP4PP":  0.95,
+    "16DP1TP1PP": 0.95,
+    "2DP2TP4PP":  0.95,
+}
+E2E_TIME_MULT = {
+    "1DP1TP1PP":  0.95,
+    "8DP1TP1PP":  0.95,
+    "1DP8TP1PP":  0.95,
+    "2DP1TP4PP":  0.95,
+    "16DP1TP1PP": 0.95,
+    "2DP2TP4PP":  0.95,
+}
 test_dir = os.path.dirname(os.path.abspath(__file__))
 baselines_dir = os.path.join(test_dir, "../PAX_MGMN")
 results_dir = os.environ.get("RESULTS_DIR")
@@ -35,7 +49,7 @@ def test_step_time(baseline_filename):
         step_time_avg_expected = json.load(baseline_file)["step_time_avg"]
         step_time_values = test_utils.read_tb_tag(event_file, step_time_summary_name).values()
         step_time_avg_actual = mean(step_time_values)
-        assert step_time_avg_expected + STEP_TIME_DELTA > step_time_avg_actual
+        assert step_time_avg_actual > step_time_avg_expected * STEP_TIME_MULT[test_config]
 
 @pytest.mark.parametrize("baseline_filename", os.listdir(baselines_dir))
 def test_e2e_time(baseline_filename):
@@ -45,5 +59,4 @@ def test_e2e_time(baseline_filename):
     with open(baseline_filepath, "r") as baseline_file:
         e2e_time_expected = json.load(baseline_file)["e2e_time_seconds"]
         e2e_time_actual = test_utils.read_e2e_time(run_log)
-        assert e2e_time_expected + E2E_TIME_DELTA > e2e_time_actual
-
+        assert e2e_time_actual < e2e_time_expected / E2E_TIME_MULT[test_config]
