7cafeb05a337a83c0630b19cb7a4f300e7e40ec1
Vladislav Kozlov <vkozlov@nvidia.com>
2023-08-25 23:47:38 +0000

    Copy libefa to /usr/lib

--- .github/container/install-efa.sh
+++ .github/container/install-efa.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
+function copy_libefa() {
+    PACKAGE_NAME=$1
+    EXTRACT_FOLDER="${PACKAGE_NAME}_"
+    # unpack downloaded package and copy libefa* stuff to /usr/lib    
+    dpkg -x ${PACKAGE_NAME}* ${EXTRACT_FOLDER}
+    # copy all libefa stuff to /usr/lib
+    for n in $(find ${EXTRACT_FOLDER}/ -name libefa*)
+    do
+        mkdir -p "$(dirname $(echo ${n} | cut -d'_' -f2-))"
+        cp -a "${n}" $(echo ${n} | cut -d'_' -f2-)
+    done
+}
+
 set -ex
 
 # Update distro
 apt-get update
 
-# Install required packages
-apt-get install -y curl
+# Workaround to have libefa* for Amazon EFA: 
+# for some reason NVIDIA/Mellanox's OFED distribution does not contain libefa*
+# Temporary solution:
+# 1. download libibverbs-dev and ibverbs-providers that contains required libefa*
+# 2. unpack them
+# 3. copy libefa to /usr/lib
 
-# Hack to install Amazon EFA correctly: for some reason the base docker 
-# image has incorrecly installed <libibverbs-dev>, as a result libefa* libraries
-# cannot be found under /usr/lib/x86_64-linux-gnu/
-# Temporary workaround: reinstall libibverbs-dev
-
-apt-get remove -y libibverbs-dev ibverbs-providers libibverbs1
-apt-get install -y libibverbs-dev
-
-# Download Amazon EFA package and install
-EFA_INSTALLER_VERSION=latest
 WORKDIR=$(mktemp -d)
-
 pushd ${WORKDIR}
 
-AMAZON_EFA_LINK="https://efa-installer.amazonaws.com/aws-efa-installer-${EFA_INSTALLER_VERSION}.tar.gz"
-curl -O "$AMAZON_EFA_LINK" 
-tar -xf aws-efa-installer-${EFA_INSTALLER_VERSION}.tar.gz && cd aws-efa-installer
-./efa_installer.sh -y --skip-kmod
+# just in case remove related packages that contains libefa
+apt-get remove -y libibverbs-dev ibverbs-providers
+
+# download deb-files without installation
+apt-get download -y libibverbs-dev ibverbs-providers
+
+copy_libefa libibverbs-dev
+copy_libefa ibverbs-providers
 
 popd
-
-# check the installation is successful
-/opt/amazon/efa/bin/fi_info --version
-
-LD_LIBRARY_PATH=/opt/amazon/efa/lib:$LD_LIBRARY_PATH
-PATH=/opt/amazon/efa/bin:$PATH
 
 # Clean up
 apt-get clean
