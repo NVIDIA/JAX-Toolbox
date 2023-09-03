0f1ffb9ba6d329693f3619f1e1c15d91019bfe85
Yu-Hang 'Maxin' Tang <Tang.Maxin@gmail.com>
2023-09-03 07:45:25 +0100

    use JSON for sitrep

--- .github/workflows/_util.sh
+++ .github/workflows/_util.sh
@@ -2,7 +2,8 @@
 
 # convert a list of variables to a json dictionary
 function to_json() {
-  jq -n \
-    $(for var in "$@"; do echo --arg $var "${!var}"; done) \
-    "{$(for var in "$@"; do echo "\"$var\": \$$var",; done)}"
+  eval $(echo jq -n \
+    $(for var in "$@"; do echo --arg $var "'"${!var}"'"; done) \
+    \'"{$(for var in "$@"; do echo -n "\"$var\": \$$var, "; done)}"\'
+  )
 }
