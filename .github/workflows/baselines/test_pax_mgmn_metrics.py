632b622237dc620d3f22d561db96bc189d58df26
Maanu Grover <109391026+maanug-nv@users.noreply.github.com>
2023-09-05 13:28:19 -0500

    Add mgmn workflow to PAX Rosetta (#206)
    - Adds pax MGMN tests (`_test_pax.yaml`) to Rosetta PAX build and test
    workflow
    - Routes pass/fail status of completion, perf, loss to badge and
    workflow output
    - add error message to asserts for perf+loss tests

--- .github/workflows/baselines/test_pax_mgmn_metrics.py
+++ .github/workflows/baselines/test_pax_mgmn_metrics.py
@@ -27,6 +27,7 @@ results_dir = os.environ.get("RESULTS_DIR")
 loss_summary_name = "loss"
 step_time_summary_name = "Steps/sec"
 
+
 @pytest.mark.parametrize("baseline_filename", os.listdir(baselines_dir))
 def test_loss(baseline_filename):
     baseline_filepath = os.path.join(baselines_dir, baseline_filename)
@@ -36,7 +37,7 @@ def test_loss(baseline_filename):
     with open(baseline_filepath, "r") as baseline_file:
         end_step = json.load(baseline_file)["end_step"]
         loss_actual = test_utils.read_tb_tag(event_file, loss_summary_name)
-        assert loss_actual[end_step] == 0
+        assert loss_actual[end_step] == 0, f"Loss at final step: {loss_actual[end_step]}, Expected loss: 0"
 
 
 @pytest.mark.parametrize("baseline_filename", os.listdir(baselines_dir))
@@ -49,7 +50,9 @@ def test_step_time(baseline_filename):
         step_time_avg_expected = json.load(baseline_file)["step_time_avg"]
         step_time_values = test_utils.read_tb_tag(event_file, step_time_summary_name).values()
         step_time_avg_actual = mean(step_time_values)
-        assert step_time_avg_actual > step_time_avg_expected * STEP_TIME_MULT[test_config]
+        assert step_time_avg_actual > step_time_avg_expected * \
+            STEP_TIME_MULT[test_config], f"Step time values: {step_time_values} (Avg: {step_time_avg_actual}), Expected avg: {step_time_avg_expected}"
+
 
 @pytest.mark.parametrize("baseline_filename", os.listdir(baselines_dir))
 def test_e2e_time(baseline_filename):
@@ -59,4 +62,5 @@ def test_e2e_time(baseline_filename):
     with open(baseline_filepath, "r") as baseline_file:
         e2e_time_expected = json.load(baseline_file)["e2e_time_seconds"]
         e2e_time_actual = test_utils.read_e2e_time(run_log)
-        assert e2e_time_actual < e2e_time_expected / E2E_TIME_MULT[test_config]
+        assert e2e_time_actual < e2e_time_expected / \
+            E2E_TIME_MULT[test_config], f"Run E2E time: {e2e_time_actual}, Expected E2E time: {e2e_time_expected}"
