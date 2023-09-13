0fc379167f3c318fcb2ad83b509a43dd31b862af
Yu-Hang Tang <Tang.Maxin@gmail.com>
2023-09-12 21:36:12 -0700

    use --arg for string and --argjson for numbers

--- .github/workflows/scripts/to_json.sh
+++ .github/workflows/scripts/to_json.sh
@@ -3,7 +3,7 @@
 # convert a list of variables to a json dictionary
 function to_json() {
   eval $(echo jq -n \
-    $(for var in "$@"; do echo --arg _$var "'"${!var}"'"; done) \
+    $(for var in "$@"; do echo $([[ "${!var}" =~ ^[0-9]+$ ]] && echo --argjson || echo --arg) _$var "'"${!var}"'"; done) \
     \'"{$(for var in "$@"; do echo -n "\"$var\": \$_$var, "; done)}"\'
   )
 }
