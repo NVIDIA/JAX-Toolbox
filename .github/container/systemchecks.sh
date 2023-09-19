173478c1d73429c17f79244b7c7fe966c1559f73
Thenerdstation <chaserileyroberts@gmail.com>
2023-09-18 23:59:42 -0700

    Moved check to base image

--- .github/container/systemchecks.sh
+++ .github/container/systemchecks.sh
@@ -3,7 +3,7 @@
 # Grab the second line / second field of the output of `df`, 
 # which is the size of shm in KBs. 
 shm_size=$(df /dev/shm | awk 'NR==2 {print $2}')
-minimum_shm_size=1048576 # ~1GB in KB
+minimum_shm_size=1048576 # ~1GB in KBs
 
 # Yellow-ish
 YELLOW='\033[0;33m'
@@ -12,16 +12,14 @@ YELLOW='\033[0;33m'
 STOPCOLOR='\033[0m'
 
 SHM_WARNING="${YELLOW}
-WARNING: Your shm is currenly less than 1GB. This may cause SIGBUS errors.\n
-To avoid this problem, you can manually set the shm size in docker with: \n
-\n
-$ docker run ... --shm-size=1g ...
-\n
-${STOPCOLOR}
-"
+WARNING: Your shm is currenly less than 1GB. This may cause SIGBUS errors.
+To avoid this problem, you can manually set the shm size in docker with:
 
-if (( shm_size < minimum_shm_size )); then
-    echo -e ${SHM_WARNING}
+$ docker run ... --shm-size=1g ...
+${STOPCOLOR}"
+
+if (( 0 < minimum_shm_size )); then
+    echo -e "$SHM_WARNING"
 fi
 
 # Run whatever command is provided by the user.
