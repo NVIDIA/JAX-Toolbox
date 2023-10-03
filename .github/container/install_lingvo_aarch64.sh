9b637468802f8165a2d5dd4fb2965e4354d1a780
Frederic Bastien <fbastien@nvidia.com>
2023-10-03 13:09:42 -0700

    Make the pax ARM dockerfile works on AMD64. Also move some stuff to the dockerfile vs install script.

--- .github/container/install_lingvo_aarch64.sh
+++ .github/container/install_lingvo_aarch64.sh
@@ -4,38 +4,28 @@ INSTALL_DIR="${INSTALL_DIR:-/opt}"
 LINGVO_REF="${LINGVO_REF:-HEAD}"
 LINGVO_REPO="${LINGVO_REPO:-https://github.com/tensorflow/lingvo.git}"
 
-## Install tensorflow-text
-cd ${INSTALL_DIR}
-pip install tensorflow_datasets==4.9.2 # force a recent version to have latest protobuf dep
-pip install auditwheel
-pip install tensorflow==2.13.0
-git clone http://github.com/tensorflow/text.git
-pushd text
-git checkout v2.13.0
-./oss_scripts/run_build.sh
-find * | grep '.whl$'
-pip install ./tensorflow_text-*.whl
-popd
-rm -Rf text
-
-## Install lingvo
+## Download lingvo early to fail fast
 LINGVO_INSTALLED_DIR=${INSTALL_DIR}/lingvo
 
 [[ -d lingvo ]] || git clone ${LINGVO_REPO} ${LINGVO_INSTALLED_DIR}
 
 pushd ${LINGVO_INSTALLED_DIR}
 # Local patches, two PR waiting to be merged + one custom patch
-git fetch origin pull/326/head:pr326
+# git fetch origin pull/326/head:pr326  ## merged upstream
 # git fetch origin pull/328/head:pr328  ## merged upstream
 git fetch origin pull/329/head:pr329
 git config user.name "JAX Toolbox"
 git config user.email "jax@toolbox"
-# git cherry-pick pr326 pr328 pr329  ## pr328 merged
-git cherry-pick pr326 pr329
+# git cherry-pick --allow-empty pr326 pr328 pr329  ## pr326 pr328 merged
+git cherry-pick --allow-empty pr329
 
 # Disable 2 flaky tests here
 patch -p1 < /opt/lingvo.patch
+popd
 
+
+## Install lingvo
+pushd ${LINGVO_INSTALLED_DIR}
 sed -i 's/tensorflow=/#tensorflow=/'  docker/dev.requirements.txt
 sed -i 's/tensorflow-text=/#tensorflow-text=/'  docker/dev.requirements.txt
 sed -i 's/dataclasses=/#dataclasses=/'  docker/dev.requirements.txt
