39bc4a4bb1d86c4d362a3eb8029c7c3e3b28b6c0
ashors1 <ashors@nvidia.com>
2023-10-10 13:38:55 -0700

    fix baseline dirs for pax tests

--- .github/workflows/baselines/test_pax_mgmn_metrics.py
+++ .github/workflows/baselines/test_pax_mgmn_metrics.py
@@ -6,24 +6,10 @@ import sys
 import test_utils
 from statistics import mean
 
-STEP_TIME_MULT = {
-    "1DP1TP1PP":  0.95,
-    "8DP1TP1PP":  0.95,
-    "1DP8TP1PP":  0.95,
-    "2DP1TP4PP":  0.95,
-    "16DP1TP1PP": 0.95,
-    "2DP2TP4PP":  0.95,
-}
-E2E_TIME_MULT = {
-    "1DP1TP1PP":  0.95,
-    "8DP1TP1PP":  0.95,
-    "1DP8TP1PP":  0.95,
-    "2DP1TP4PP":  0.95,
-    "16DP1TP1PP": 0.95,
-    "2DP2TP4PP":  0.95,
-}
+STEP_TIME_MULT = 0.95
+E2E_TIME_MULT = 0.95
 test_dir = os.path.dirname(os.path.abspath(__file__))
-baselines_dir = os.path.join(test_dir, sys.argv[1])
+baselines_dir = os.path.join(test_dir, os.environ.get("BASELINES_DIR"))
 results_dir = os.environ.get("RESULTS_DIR")
 loss_summary_name = "loss"
 step_time_summary_name = "Steps/sec"
@@ -52,7 +38,7 @@ def test_step_time(baseline_filename):
         step_time_values = test_utils.read_tb_tag(event_file, step_time_summary_name).values()
         step_time_avg_actual = mean(step_time_values)
         assert step_time_avg_actual > step_time_avg_expected * \
-            STEP_TIME_MULT[test_config], f"Step time values: {step_time_values} (Avg: {step_time_avg_actual}), Expected avg: {step_time_avg_expected}"
+            STEP_TIME_MULT, f"Step time values: {step_time_values} (Avg: {step_time_avg_actual}), Expected avg: {step_time_avg_expected}"
 
 
 @pytest.mark.parametrize("baseline_filename", os.listdir(baselines_dir))
@@ -64,4 +50,4 @@ def test_e2e_time(baseline_filename):
         e2e_time_expected = json.load(baseline_file)["e2e_time_seconds"]
         e2e_time_actual = test_utils.read_e2e_time(run_log)
         assert e2e_time_actual < e2e_time_expected / \
-            E2E_TIME_MULT[test_config], f"Run E2E time: {e2e_time_actual}, Expected E2E time: {e2e_time_expected}"
+            E2E_TIME_MULT, f"Run E2E time: {e2e_time_actual}, Expected E2E time: {e2e_time_expected}"
