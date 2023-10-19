de7b6cf94213ff9ffc3600863e67c06c563b77a9
Olli Lupton <olupton@nvidia.com>
2023-10-19 16:29:29 +0200

    prefer skopeo to REST

--- .github/workflows/scripts/get_remote_env.sh
+++ .github/workflows/scripts/get_remote_env.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-source $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/inspect_remote_img.sh
 get_remote_env() {
-  if [[ $# -ne 4 ]]; then
-    echo 'get_remote_env $GH_TOKEN $IMAGE $OS $ARCH'
-    echo 'Example: get_remote_env XXXXXXXXXXXX ghcr.io/nvidia/upstream-t5x:latest linux amd64'
+  if [[ $# -ne 3 ]]; then
+    echo 'get_remote_env $IMAGE $OS $ARCH'
+    echo 'Example: get_remote_env ghcr.io/nvidia/upstream-t5x:latest linux amd64'
     echo 'Returns the ENV of a tagged remote image (no download)'
     echo 'Useful to inspect CUDA env vars'
     return 1
   fi
-  inspect_remote_img $@ | jq .config.Env
+  skopeo inspect --override-arch "${3}" --override-os "${2}" "docker://${1}"  | jq -r '.Env'
 }
