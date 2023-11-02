ad83f219d79aa1861b6d9fc8fa10dce0689cc96f
Yu-Hang Tang <Tang.Maxin@gmail.com>
2023-11-02 18:26:54 +0000

    hack for git top of tree Flax dependency

--- .github/container/pip-finalize.sh
+++ .github/container/pip-finalize.sh
@@ -6,6 +6,8 @@ sed -i "s|flax @ git+https://github.com/google/flax#egg=flax||g" /opt/pip-tools.
 
 pip-compile $(ls /opt/pip-tools.d/manifest.*) -o /opt/pip-tools.d/requirements.txt
 
+sed -i "s|flax @ git+https://github.com/google/flax.git|-e /opt/flax|g" /opt/pip-tools.d/requirements.txt
+
 pip-sync /opt/pip-tools.d/requirements.txt
 
 rm -rf ~/.cache/*
