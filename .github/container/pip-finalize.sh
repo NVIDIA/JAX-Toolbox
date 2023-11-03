b7e1a6cd5842bc9fe95d86cf03325ec8f2f56d29
Yu-Hang Tang <Tang.Maxin@gmail.com>
2023-11-03 17:43:51 +0000

    update URL req

--- .github/container/pip-finalize.sh
+++ .github/container/pip-finalize.sh
@@ -2,12 +2,8 @@
 
 set -ex -o pipefail
 
-sed -i "s|flax @ git+https://github.com/google/flax#egg=flax||g" /opt/pip-tools.d/manifest.*
-
 pip-compile $(ls /opt/pip-tools.d/manifest.*) -o /opt/pip-tools.d/requirements.txt
 
-sed -i "s|flax @ git+https://github.com/google/flax.git|-e /opt/flax|g" /opt/pip-tools.d/requirements.txt
-
-pip-sync /opt/pip-tools.d/requirements.txt
+pip install --src /opt -r /opt/pip-tools.d/requirements.txt
 
 rm -rf ~/.cache/*
