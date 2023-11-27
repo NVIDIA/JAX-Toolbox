83c91385cf3836f52621df5de2bbce0d4d492d10
Olli Lupton <olupton@nvidia.com>
2023-11-27 13:38:21 +0100

    Update .github/workflows/scripts/get_remote_labels.sh
    Co-authored-by: Terry Kong <terryk@nvidia.com>

--- .github/workflows/scripts/get_remote_labels.sh
+++ .github/workflows/scripts/get_remote_labels.sh
@@ -3,7 +3,7 @@
 source $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/inspect_remote_img.sh
 get_remote_labels() {
   if [[ $# -ne 4 ]]; then
-    echo '[get_remote_labels](./get_remote_labels.sh) $GH_TOKEN $IMAGE $OS $ARCH'
+    echo 'get_remote_labels $GH_TOKEN $IMAGE $OS $ARCH'
     echo 'Example: get_remote_labels XXXXXXXXXXXX ghcr.io/nvidia/upstream-t5x:latest linux amd64'
     echo 'Returns the opencontainer annotation labels of a tagged remote image (no download)'
     return 1
