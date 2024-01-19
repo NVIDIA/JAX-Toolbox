120b6ddee6f421b4b39f5f91714f00716fd969d9
Vladislav Kozlov <vkozlov@nvidia.com>
2024-01-18 22:42:58 -0700

    Add warning about model cache

---
+++ .github/container/levanter-cache-warn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+YELLOW='\033[0;33m'
+NOCOLOR='\033[0m'
+
+echo -e "${YELLOW}
+WARNING: To speed up the model training it is recommended to cache the datasets
+inside docker container with:
+
+$ python -m levanter.main.cache_dataset --id <dataset_id> --cache_dir cache/
+
+If you have already done this, please feel free to ignore the message above.
+${NOCOLOR}"
