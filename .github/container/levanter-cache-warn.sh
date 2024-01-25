1e4d8dd1b00a824c6e23e1c5ce4d412cb1ac65a8
Vladislav Kozlov <vkozlov@nvidia.com>
2024-01-24 17:05:26 -0700

    Add Levanter

--- .github/container/levanter-cache-warn.sh
+++ .github/container/levanter-cache-warn.sh
@@ -4,10 +4,15 @@ YELLOW='\033[0;33m'
 NOCOLOR='\033[0m'
 
 echo -e "${YELLOW}
-WARNING: To speed up the model training it is recommended to cache the datasets
-inside docker container with:
+WARNING: To speed up the model training it is recommended to cache the datasets with:
 
-    $ python -m levanter.main.cache_dataset --id <dataset_id> --cache_dir cache/
+    $ python -m levanter.main.cache_dataset --id <dataset_id> --cache_dir <path/to/shared/cache>
 
-If you have already done this, please feel free to disregard the message.
+When run this container on distributed system, please make sure that the cache folder 
+(<path/to/shared/cache>) is on a shared filesystem across nodes.
+To supply the cache folder, you can use `--data.cache_dir` flag for Levanter, for example:
+
+    $ docker run -v <path/to/shared/cache>:<path/to/in-docker/cache> <levanter-docker-tag> \\
+        python -m levanter.main.train_lm --data.cache_dir <path/to/in-docker/cache>\"
+
 ${NOCOLOR}"
