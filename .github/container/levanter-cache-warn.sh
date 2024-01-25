0c8cb3ab0141b549d276a10ed5a16da2f89450e8
Vladislav Kozlov <vkozlov@nvidia.com>
2024-01-24 17:50:48 -0700

    Add Levanter, not unit test for now

--- .github/container/levanter-cache-warn.sh
+++ .github/container/levanter-cache-warn.sh
@@ -8,11 +8,18 @@ WARNING: To speed up the model training it is recommended to cache the datasets 
 
     $ python -m levanter.main.cache_dataset --id <dataset_id> --cache_dir <path/to/shared/cache>
 
-When run this container on distributed system, please make sure that the cache folder 
-(<path/to/shared/cache>) is on a shared filesystem across nodes.
-To supply the cache folder, you can use `--data.cache_dir` flag for Levanter, for example:
+When run on distributed system, please make sure that the cache folder <path/to/shared/cache>
+is on a network/shared filesystem across nodes. 
 
-    $ docker run -v <path/to/shared/cache>:<path/to/in-docker/cache> <levanter-docker-tag> \\
-        python -m levanter.main.train_lm --data.cache_dir <path/to/in-docker/cache>\"
+To supply the cache folder to Levanter, use `--data.cache_dir` flag:
+
+    $ docker run -v <path/to/shared/cache>:<path/to/in-docker/cache> <levanter-docker-image> \\
+        python -m levanter.main.train_lm <...> --data.cache_dir <path/to/in-docker/cache>
+
+Alternatevely, you can skip `--data.cache_dir` flag, then the cache folder is `/opt/levanter/cache`
+be default:
+
+    $ docker run -v <path/to/shared/cache>:/opt/levanter/cache <levanter-docker-image> \\
+        python -m levanter.main.train_lm <...>
 
 ${NOCOLOR}"
