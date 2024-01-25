60b5b94b26518b886f517f03eb5abeefdc9bda58
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2024-01-24 22:24:10 -0800

    Fix typo

--- .github/container/levanter-cache-warn.sh
+++ .github/container/levanter-cache-warn.sh
@@ -9,8 +9,9 @@ the cache default to ~/.cache but can be changed via the `--data.cache_dir` flag
 
     $ python -m levanter.main.train_lm ... --data.cache_dir <path/to/cache>
 
-When running on a distributed system, please make sure that the cache folder is placed on a network-readable path,
-e.g. a path in an object store (e.g. GCS). or a path to a pre-built cache., that is accessible by all participating nodes.
+When running on a distributed system, please make sure that the cache folder is placed on a network-readable path
+that is accessible by all participating nodes, e.g. an object store (e.g. GCS), a network file system,
+or a path to a pre-built cache.
 
     $ docker run -v <path/to/shared/filesystem>:<path/to/cache> <levanter-docker-image> ...
     OR
