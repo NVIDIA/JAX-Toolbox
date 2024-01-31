243e4f61304cca3f8863b24e39676b70dc5e65b9
Md Khan <mdfahimfaysa@sc-xterm-306.nvidia.com>
2024-01-30 17:44:50 -0800

    added multiprocess support

--- .github/container/test-maxtext.sh
+++ .github/container/test-maxtext.sh
@@ -18,6 +18,7 @@ usage() {
     echo "  --enable-te                If set, will run with env var ENABLE_TE=1."
     echo "  --enable-fused-attn        If set, will run with env var NVTE_FUSED_ATTN=1." 
     echo "  -s, --steps                Number of steps to run, defaults to 500."
+    echo "  --multiprocess             Enable the multiprocess GPU mode."
     echo "  -o, --output NAME          Name for the output folder, a temporary folder will be created if none specified."
     echo "  --data-parallel            Data parallelism to use. Defaults to 1."
     echo "  --fsdp                     Fully-sharded data parallelism to use. Defaults to 1."
@@ -28,12 +29,13 @@ usage() {
     exit $1
 }
 
-args=$(getopt -o a:b:s:o:n:h --long additional-args:,batch-per-gpu:,dtype:,enable-te,enable-fused-attn,steps:,help,output:,data-parallel:,fsdp:,tensor-parallel:,pipeline-parallel:,nodes: -- "$@")
+args=$(getopt -o a:b:s:o:n:h --long additional-args:,batch-per-gpu:,dtype:,enable-te,enable-fused-attn,steps:,help,multiprocess,output:,data-parallel:,fsdp:,tensor-parallel:,pipeline-parallel:,nodes: -- "$@")
 if [[ $? -ne 0 ]]; then
     exit $1
 fi
 
 # Default arguments
+MULTIPROCESS=false
 OUTPUT=$(mktemp -d)
 
 BATCH_PER_GPU=2
@@ -70,6 +72,10 @@ while [ : ]; do
         -s | --steps)
             STEPS="$2"
             shift 2
+            ;;
+        --multiprocess)
+            MULTIPROCESS=true
+            shift 1
             ;;
         -o | --output)
             OUTPUT=$2
@@ -117,6 +123,7 @@ print_var BATCH_PER_GPU
 print_var DTYPE
 print_var STEPS
 print_var NGPUS
+print_var MULTIPROCESS
 print_var OUTPUT
 print_var ENABLE_TE
 print_var ENABLE_FUSED_ATTN
@@ -131,6 +138,7 @@ pushd ${MAXTEXT_DIR}
 ## Launch
 set -ex
 
+export NVTE_FUSED_ATTN=${ENABLE_FUSED_ATTN}
 export XLA_PYTHON_CLIENT_MEM_FRACTION=0.65
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
@@ -147,7 +155,7 @@ RUN_NAME="500M_PP${PP}_DP${DP}_FSDP${FSDP}_TP${TP}"
 RUN_SETTINGS="MaxText/train.py MaxText/configs/base.yml run_name=${RUN_NAME}\
     steps=$STEPS per_device_batch_size=2 base_emb_dim=2560 base_mlp_dim=8192 remat_policy=minimal\
     base_num_query_heads=8 base_num_kv_heads=8 base_num_decoder_layers=8 head_dim=128 enable_checkpointing=false\
-    base_output_directory=$OUTPUT dataset_path=local dataset_type=synthetic\
+    base_output_directory=$OUTPUT dataset_path=local dataset_type=synthetic multiprocess_gpu=${MULTIPROCESS}\
     dcn_fsdp_parallelism=1 ici_fsdp_parallelism=$FSDP\
     ici_data_parallelism=$DP dcn_data_parallelism=1\
     ici_tensor_parallelism=$TP dcn_tensor_parallelism=1"
