600d77aa69a36ce5d98e93f72b8eed51966626c2
Olli Lupton <olupton@nvidia.com>
2024-02-13 18:46:54 +0100

    deduplicate following code review

--- .github/container/install-nsight.sh
+++ .github/container/install-nsight.sh
@@ -2,10 +2,11 @@
 set -ex -o pipefail
 
 # Repo for newer nsight versions
-export UBUNTU_ARCH=$(dpkg --print-architecture)
-export UBUNTU_VERSION=$(. /etc/os-release && echo ${ID}${VERSION_ID/./}) # e.g. ubuntu2204
-curl -o /usr/share/keyrings/nvidia.pub https://developer.download.nvidia.com/devtools/repos/${UBUNTU_VERSION}/${UBUNTU_ARCH}/nvidia.pub
-echo "deb [signed-by=/usr/share/keyrings/nvidia.pub] https://developer.download.nvidia.com/devtools/repos/${UBUNTU_VERSION}/${UBUNTU_ARCH}/ /" > /etc/apt/sources.list.d/devtools-${UBUNTU_VERSION}-${UBUNTU_ARCH}.list
+UBUNTU_ARCH=$(dpkg --print-architecture)
+UBUNTU_VERSION=$(. /etc/os-release && echo ${ID}${VERSION_ID/./}) # e.g. ubuntu2204
+DEVTOOLS_URL=https://developer.download.nvidia.com/devtools/repos/${UBUNTU_VERSION}/${UBUNTU_ARCH}
+curl -o /usr/share/keyrings/nvidia.pub "${DEVTOOLS_URL}/nvidia.pub"
+echo "deb [signed-by=/usr/share/keyrings/nvidia.pub] ${DEVTOOLS_URL}/ /" > /etc/apt/sources.list.d/devtools-${UBUNTU_VERSION}-${UBUNTU_ARCH}.list
 
 export DEBIAN_FRONTEND=noninteractive
 export TZ=America/Los_Angeles
