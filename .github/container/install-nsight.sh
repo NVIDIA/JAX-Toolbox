ebacfbba87c9e2dcc190fd812e7f64869647f7a2
Olli Lupton <olupton@nvidia.com>
2024-02-15 16:25:43 +0000

    Prefer libcupti.so.12.3 to libcupti.so.12.4

--- .github/container/install-nsight.sh
+++ .github/container/install-nsight.sh
@@ -16,3 +16,17 @@ apt-get install -y nsight-compute nsight-systems-cli
 apt-get clean
 
 rm -rf /var/lib/apt/lists/*
+
+# "Wrong event order has been detected when adding events to the collection"
+# workaround during nsys report post-processing with 2024.1.1 and CUDA 12.3
+NSYS202411=/opt/nvidia/nsight-systems-cli/2024.1.1
+if [[ -d "${NSYS202411}" ]]; then
+  LIBCUPTI123=/opt/nvidia/nsight-compute/2023.3.0/host/target-linux-x64/libcupti.so.12.3
+  if [[ ! -f "${LIBCUPTI123}" ]]; then
+    echo "2024.1.1 workaround expects to be running inside 12.3.0 container"
+    exit 1
+  fi
+  # Use libcupti.so.12.3 because this is a CUDA 12.3 container
+  ln -s "${LIBCUPTI123}" "${NSYS202411}/target-linux-x64/libcupti.so.12.3"
+  mv "${NSYS202411}/target-linux-x64/libcupti.so.12.4" "${NSYS202411}/target-linux-x64/_libcupti.so.12.4"
+fi
