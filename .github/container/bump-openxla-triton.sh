d9bf13169c4811c3f2fd6a4fc2122978d6de5306
Olli Lupton <olupton@nvidia.com>
2024-02-20 10:45:01 +0000

    Pallas: apply Triton patches from XLA

--- .github/container/bump-openxla-triton.sh
+++ .github/container/bump-openxla-triton.sh
@@ -6,7 +6,7 @@ usage() {
 cat <<EOF
 This script is a utility for updating the commit reference for openxla-triton
 in a manifest YAML file used to build JAX-Toolbox images. The commit is derived
-from the commit for xla contained in the manifest.
+from the commit for xla contained in the manifest, along with the patches.
 
 Usage: $0 [OPTION]...
   -h, --help      Print usage.
@@ -50,6 +50,7 @@ if [[ -z "${MANIFEST:-}" ]]; then
 fi
 
 set -eou pipefail
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 xla_url=$(yq e ".xla.url" $MANIFEST)
 xla_tracking_ref=$(yq e ".xla.tracking_ref" $MANIFEST)
@@ -58,8 +59,18 @@ xla_commit=$(yq e ".xla.latest_verified_commit" $MANIFEST)
 git clone --branch "${xla_tracking_ref}" --single-branch "${xla_url}" "${xla_repo}"
 (cd "${xla_repo}" && git checkout "${xla_commit}")
 # Extract the openxla/triton tag used by XLA. Even though it is called
-# TRITON_COMMIT it is a tag. In principle we should also account for the
-# patches in this .bzl file, but skip that for now.
-openxla_triton_tag=$(sed -n -e 's#\s\+TRITON_COMMIT = "\(cl[0-9]\+\)"#\1#p' "${xla_repo}/third_party/triton/workspace.bzl")
+# TRITON_COMMIT it is a tag.
+workspace_file="${xla_repo}/third_party/triton/workspace.bzl"
+openxla_triton_tag=$(sed -n -e 's#\s\+TRITON_COMMIT = "\(cl[0-9]\+\)"#\1#p' "${workspace_file}")
+# Extract Triton patch files applied by XLA
+patch_files=$(python3 -c 'import ast, sys; tree = ast.parse(sys.stdin.read()); print(" ".join(elem.value.removeprefix("//third_party/triton:").removesuffix(".patch") for node in ast.walk(tree) if isinstance(node, ast.keyword) and node.arg == "patch_file" for elem in node.value.elts))' < "${workspace_file}")
+# Clear old patches from the manifest file
+yq e 'del(.openxla-triton.patches)' -i "${MANIFEST}"
+i=0
+for patch_file in ${patch_files}; do
+  cp -v "${xla_repo}/third_party/triton/${patch_file}.patch" "${SCRIPT_DIR}/patches/openxla-triton/${i}_${patch_file}.patch"
+  yq e ".openxla-triton.patches.${patch_file} = \"file://patches/openxla-triton/${i}_${patch_file}.patch\"" -i "${MANIFEST}"
+  i=$((i+1))
+done
 rm -rf "${xla_repo}"
 yq e ".openxla-triton.latest_verified_commit = \"${openxla_triton_tag}\"" -i $MANIFEST
