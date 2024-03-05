1d474918b1aa4db676b115c3bfed25d780211fab
Yu-Hang 'Maxin' Tang <Tang.Maxin@gmail.com>
2024-03-05 19:56:41 +0000

    debug

--- .github/workflows/scripts/wait_for_slurm_job.sh
+++ .github/workflows/scripts/wait_for_slurm_job.sh
@@ -2,22 +2,24 @@
 
 # waits for a Slurm job to complete, fail, or be cancelled.
 function wait_for_slurm_job() {
+
+    opts=$(set +o)
+    set +x
+
     host=$1
     job_id=$2
     check_every=${3:-15}
 
     while true; do
-        status=$(ssh $host squeue --job $job_id --noheader --format=%T)
-
-        echo "status = '$status'"
+        status=$(ssh $host squeue --job $job_id --noheader --format=%T 2>/dev/null || echo "SSH error: $?")
+        echo "[$(date)] job $job_id: $status"
 
         if [ -z "$status" ]; then
             break
-        else
-            echo "[$(date)] job $job_id: $status"
         fi
 
-        # Wait for a bit before checking again
         sleep $check_every
     done
+
+    eval "$opts"
 }
