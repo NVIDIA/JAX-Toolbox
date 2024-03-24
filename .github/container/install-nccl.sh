ed162357d5a24acc1bc008ef369a77eff8863f3e
Vladislav <vkozlov@nvidia.com>
2024-03-23 22:44:53 -0600

    JAX_NCCL_VERSION -> NCCL_VERSION [skip ci]

--- .github/container/install-nccl.sh
+++ .github/container/install-nccl.sh
@@ -7,7 +7,6 @@ export TZ=America/Los_Angeles
 
 apt-get update
 
-
 # Extract CUDA version from `nvcc --version` output line
 # Input: "Cuda compilation tools, release X.Y, VX.Y.Z"
 # Output: X.Y
@@ -15,13 +14,10 @@ cuda_version=$(nvcc --version | sed -n 's/^.*release \([0-9]*\.[0-9]*\).*$/\1/p'
 
 # Find latest NCCL version compatible with existing CUDA by matching
 # ${cuda_version} in the package version string
-libnccl2_version=$(apt-cache show libnccl-dev | sed -n "s/^Version: \(${NCCL_VERSION}+cuda.*\)$/\1/p" | head -n 1)
-libnccl_dev_version=$(apt-cache show libnccl-dev | sed -n "s/^Version: \(${NCCL_VERSION}+cuda.*\)$/\1/p" |head -n 1)
-
-requested_cuda_version=${libnccl_dev_version##*+cuda}
-
-if [[ $requested_cuda_version != $cuda_version ]]; then
-    echo "Could not find compatible NCCL version $NCCL_VERSION for CUDA $cuda_version. Available only $libnccl_dev_version"
+libnccl2_version=$(apt-cache show libnccl-dev | sed -n "s/^Version: \(${NCCL_VERSION}.*+cuda${cuda_version}\)$/\1/p" | head -n 1)
+libnccl_dev_version=$(apt-cache show libnccl-dev | sed -n "s/^Version: \(${NCCL_VERSION}.*+cuda${cuda_version}\)$/\1/p" | head -n 1)
+if [[ -z "${libnccl2_version}" || -z "${libnccl_dev_version}" ]]; then
+    echo "Could not find compatible NCCL version for CUDA ${cuda_version}"
     exit 1
 fi
 
