558453d43725fd23d653ddf482df980c74c40405
Yu-Hang 'Maxin' Tang <Tang.Maxin@gmail.com>
2024-04-12 23:13:25 +0000

    automatically config NICs used by NCCL on GCP

--- .github/container/gcp-autoconfig.sh
+++ .github/container/gcp-autoconfig.sh
@@ -10,6 +10,24 @@ NOCOLOR='\033[0m'
 
 # If google.internal exists, we are most likely running on GCP.
 if host "google.internal" > /dev/null; then
+    echo -e "${GREEN}
+    ========================== JAX-ToolBox on GCP ==========================
+
+    It looks like you're running on GCP. In order to maximize your multi-node performance,
+    you'll need to use Google's TCPx NCCL plugin. This container ships the plugin at
+    ${TCPX_LIBRARY_PATH}, which is already added to LD_LIBRARY_PATH by this script $1.
+
+    However, there are additional steps you will need to take. Mainly, you'll need to run a 
+    separate receive-datapath-manager daemon on each of your nodes, and correctly configure your 
+    networks and NICs.
+
+    For more information, please see the guide at: 
+    https://cloud.google.com/compute/docs/gpus/gpudirect#provide-access
+
+    If you believe this setup is causing undesired effects, you can disable it
+    by setting DISABLE_GCP_TCPX_SETUP=1
+    ${NOCOLOR}"
+
     export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${TCPX_LIBRARY_PATH}"
     export NCCL_NET=GPUDirectTCPX_v7
 
@@ -36,22 +54,4 @@ if host "google.internal" > /dev/null; then
     export NCCL_GPUDIRECTTCPX_FORCE_ACK=0
 
     echo "[GCP autoconfig $(hostname)] NCCL_SOCKET_IFNAME=${NCCL_SOCKET_IFNAME} NCCL_GPUDIRECTTCPX_CTRL_DEV=${NCCL_GPUDIRECTTCPX_CTRL_DEV} NCCL_GPUDIRECTTCPX_SOCKET_IFNAME=${NCCL_GPUDIRECTTCPX_SOCKET_IFNAME}"
-
-    echo -e "${GREEN}
-    ========================== JAX-ToolBox on GCP ==========================
-
-    It looks like you're running on GCP. In order to maximize your multi-node performance,
-    you'll need to use Google's TCPx NCCL plugin. This container ships the plugin at
-    ${TCPX_LIBRARY_PATH}, which is already added to LD_LIBRARY_PATH by this script $1.
-
-    However, there are additional steps you will need to take. Mainly, you'll need to run a 
-    separate receive-datapath-manager daemon on each of your nodes, and correctly configure your 
-    networks and NICs.
-
-    For more information, please see the guide at: 
-    https://cloud.google.com/compute/docs/gpus/gpudirect#provide-access
-
-    If you believe this setup is interfering with your work, you can disable it
-    by setting DISABLE_GCP_TCPX_SETUP=1
-    ${NOCOLOR}"
 fi
