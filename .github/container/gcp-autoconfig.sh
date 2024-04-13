fb4a93a12c149ba5717cabcb181379cd758c5ad5
Yu-Hang 'Maxin' Tang <Tang.Maxin@gmail.com>
2024-04-13 06:00:51 +0000

    improve entrypoint message

--- .github/container/gcp-autoconfig.sh
+++ .github/container/gcp-autoconfig.sh
@@ -11,22 +11,24 @@ NOCOLOR='\033[0m'
 # If google.internal exists, we are most likely running on GCP.
 if host metadata.google.internal &> /dev/null; then
     echo -e "${GREEN}
-    ========================== JAX-ToolBox on GCP ==========================
+============================== JAX-ToolBox on GCP ==============================
 
-    It looks like you're running on GCP. In order to maximize your multi-node performance,
-    you'll need to use Google's TCPx NCCL plugin. This container ships the plugin at
-    ${TCPX_LIBRARY_PATH}, which is already added to LD_LIBRARY_PATH by this script $1.
+It looks like you're running on GCP. In order to maximize your multi-node
+performance, you'll need to use Google's TCPx NCCL plugin. This container ships
+the plugin at ${TCPX_LIBRARY_PATH}, which is already added to LD_LIBRARY_PATH by
+this script $2.
 
-    However, there are additional steps you will need to take. Mainly, you'll need to run a 
-    separate receive-datapath-manager daemon on each of your nodes, and correctly configure your 
-    networks and NICs.
+For A3 instances, this script also automatically determines the best NICs to use
+by NCCL for maximum performance.
 
-    For more information, please see the guide at: 
-    https://cloud.google.com/compute/docs/gpus/gpudirect#provide-access
+However, there are additional steps you will need to take. Mainly, you'll need
+to run a separate receive-datapath-manager daemon on each of your nodes, and
+correctly configure your networks and NICs. For more information, please see the
+guide at: https://cloud.google.com/compute/docs/gpus/gpudirect#provide-access
 
-    If you believe this setup is causing undesired effects, you can disable it
-    by setting DISABLE_GCP_TCPX_SETUP=1
-    ${NOCOLOR}"
+If you believe this setup is causing undesired effects, you can disable it by
+setting DISABLE_GCP_TCPX_SETUP=1
+${NOCOLOR}"
 
     export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${TCPX_LIBRARY_PATH}"
     export NCCL_NET=GPUDirectTCPX_v7
