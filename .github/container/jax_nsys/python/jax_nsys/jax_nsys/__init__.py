46ab5a1216bc1e40ca3010ac87c7f3a9fab4eaab
Olli Lupton <olupton@nvidia.com>
2024-04-25 11:32:16 +0200

    nsys-jax: add profiling wrapper script (#704)
    This wraps `nsys profile` and writes a .zip archive containing
    additional information for offline analysis of JAX/XLA profiles:
    - All Python source files referred to in the JAX/XLA metadata.
    - XLA metadata exported in .pb and .pbtxt format.
    - .proto schema files needed to read the above.
    
    In addition, the .nsys-rep file is compressed inside the .zip archive,
    and a .parquet format export of the NVTX trace is included.
    
    The wrapper also ensures that the metadata does not truncate Python
    stack traces, and enables CUDA graph node tracing -- although at present
    disabling CUDA graphs is recommended to avoid limitations in metadata
    correlation.

---
+++ .github/container/jax_nsys/python/jax_nsys/jax_nsys/__init__.py
@@ -0,0 +1,4 @@
+from .data_loaders import load_profiler_data
+from .protobuf import xla_module_metadata
+from .protobuf_utils import compile_protos
+from .visualization import create_flamegraph, display_flamegraph
---
+++ .github/container/jax_nsys/python/jax_nsys/jax_nsys/protobuf_utils.py
@@ -0,0 +1,42 @@
+# WARNING: it is tacitly assumed that the protobuf compiler (protoc) is
+# compatible with the google.protobuf version.
+import glob
+import google.protobuf
+import os
+import pathlib
+import shutil
+import subprocess
+import sys
+
+
+def which(executable: str) -> pathlib.Path:
+    """
+    Wrap shutil.which, making sure that if we are inside a virtual environment
+    then the bin/ directory is searched first.
+    """
+    # If we are running in a virtual environment, make sure the bin/ directory
+    # of it is in the PATH.
+    path = os.environ.get("PATH", os.defpath).split(":")
+    if sys.prefix != sys.base_prefix:
+        # Running in a virtual environment
+        venv_bin = os.path.join(sys.prefix, "bin")
+        if venv_bin not in path:
+            path.insert(0, venv_bin)
+    return shutil.which(executable, path=":".join(path))
+
+
+def compile_protos(proto_dir: pathlib.Path, output_dir: pathlib.Path):
+    if not os.path.isdir(proto_dir):
+        raise Exception(f"Input: {proto_dir} is not a directory")
+    if not os.path.isdir(output_dir):
+        raise Exception(f"Output: {output_dir} is not a directory")
+    # Find the .proto files
+    proto_files = glob.glob("**/*.proto", recursive=True, root_dir=proto_dir)
+    if len(proto_files) == 0:
+        raise Exception(f"Did not find any .proto files under {proto_dir}")
+    protoc = which("protoc")
+    # Generate code to load the protobuf files
+    subprocess.run(
+        [protoc, f"-I={proto_dir}", f"--python_out={output_dir}"] + proto_files,
+        check=True,
+    )
