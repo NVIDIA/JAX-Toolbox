bf9f371b8f37688ce4c3f691c5fac6bb2783c605
Georg Stefan Schmid <gschmid@nvidia.com>
2024-05-10 05:54:00 -0700

    More fixes

--- .github/container/install-nccl-sanity-check.sh
+++ .github/container/install-nccl-sanity-check.sh
@@ -13,10 +13,7 @@ cat <<"EOF" > "$BIN_DIR/$NAME.sh"
 #!/bin/bash
 set -e
 export NCCL_SANITY_CHECK_LATENCY_US=1000
-NCCL_SANITY_CHECK_ARGS=$(python3 -c 'import jax; jax.distributed.initialize(); from jax._src.distributed import global_state as gs; print(gs.process_id, gs.num_processes, gs.coordinator_address)')
+NCCL_SANITY_CHECK_ARGS=$(python3 -c 'import jax; import jax.distributed; jax.distributed.initialize(); lds = jax.local_devices(); assert(len(lds) == 1); from jax._src.distributed import global_state as gs; print(gs.num_processes, gs.process_id, lds[0].local_hardware_id, gs.coordinator_address)')
 nccl-sanity-check $NCCL_SANITY_CHECK_ARGS
 EOF
 chmod +x "$BIN_DIR/$NAME.sh"
-
-# REMOVE ME: To test with base container (rather than jax-from-source container)
-pip3 install jax
