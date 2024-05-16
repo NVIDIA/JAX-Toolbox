5079c987c8616b75a28061e61fc1319fddc5bebd
Md Fahim Faysal Khan <mdfahimfaysa@nvidia.com>
2024-05-16 11:38:15 -0700

    fixed XLA flag for precision

--- .github/container/test-maxtext.sh
+++ .github/container/test-maxtext.sh
@@ -187,7 +187,6 @@ export CUDA_DEVICE_MAX_CONNECTIONS=1
 
 export BASE_XLA_FLAGS=${BASE_XLA_FLAGS:---xla_gpu_enable_latency_hiding_scheduler=true 
                 --xla_gpu_enable_triton_gemm=false
-                --xla_allow_excess_precision
                 --xla_gpu_graph_level=0 
                 --xla_gpu_enable_highest_priority_async_stream=true
                 --xla_gpu_all_reduce_combine_threshold_bytes=1073741824 
