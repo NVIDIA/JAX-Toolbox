f0217b6e5e06cbe8320d5d9f6100638b3ed17b58
Vladislav <vkozlov@nvidia.com>
2024-05-22 04:53:12 -0600

    Install bazelisk as /usr/local/bin/bazel (#841)
    Add `bazelisk` to manage what version of `bazel` to use on the fly
    
    ---------
    
    Co-authored-by: Olli Lupton <olupton@nvidia.com>

--- .github/container/test-jax.sh
+++ .github/container/test-jax.sh
@@ -27,8 +27,7 @@ jax_source_dir() {
 query_tests() {
     cd `jax_source_dir`
     python build/build.py --configure_only
-    BAZEL=$(find -type f -executable -name "bazel-*")
-    $BAZEL query tests/... 2>&1 | grep -F '//tests:'
+    bazel query tests/... 2>&1 | grep -F '//tests:'
     exit
 }
 
@@ -185,5 +184,4 @@ pip install matplotlib
 
 cd `jax_source_dir`
 python build/build.py --configure_only
-BAZEL=$(find -type f -executable -name "bazel-*")
-$BAZEL test ${BAZEL_TARGET} ${COMMON_FLAGS} ${EXTRA_FLAGS}
+bazel test ${BAZEL_TARGET} ${COMMON_FLAGS} ${EXTRA_FLAGS}
