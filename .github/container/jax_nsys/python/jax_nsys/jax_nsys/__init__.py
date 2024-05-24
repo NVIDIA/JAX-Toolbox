0113abfa79da7ae24425dd38686e01fba5eee985
Olli Lupton <olupton@nvidia.com>
2024-05-24 10:51:04 +0200

    nsys-jax: handle parallel compilation (#822)
    Improve handling of parallel compilation. Now, range hierarchies in
    worker threads are spliced in under the main thread ranges that launch
    them. This allows a meaningful summary of where compilation time is
    spent even when parallelism is enabled.
    
    Also support profiles from runs with multiple GPUs per process.

--- .github/container/jax_nsys/python/jax_nsys/jax_nsys/__init__.py
+++ .github/container/jax_nsys/python/jax_nsys/jax_nsys/__init__.py
@@ -1,4 +1,9 @@
+import pandas as _pd
+_pd.options.mode.copy_on_write = True
+
+from .analysis import generate_compilation_statistics
 from .data_loaders import load_profiler_data
 from .protobuf import xla_module_metadata
 from .protobuf_utils import compile_protos
+from .utils import remove_child_ranges
 from .visualization import create_flamegraph, display_flamegraph
