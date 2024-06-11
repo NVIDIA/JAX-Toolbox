19cc606658dfcefa60de10a8387d1dc7b9f41690
Vladislav <vkozlov@nvidia.com>
2024-06-10 21:19:19 -0700

    Run sandbox with debugging

--- .github/workflows/scripts/docker_size.sh
+++ .github/workflows/scripts/docker_size.sh
@@ -1,4 +1,13 @@
 function dockersize() { 
+    docker manifest inspect -v "$1"
+    echo "**************************"
+    docker manifest inspect -v "$1" \
+    | jq -c 'if type == "array" then .[] else . end'
+    echo "**************************"
+        docker manifest inspect -v "$1" \
+    | jq -c 'if type == "array" then .[] else . end' \
+    | jq -r '[ ( .Descriptor.platform | [ .os, .architecture, .variant, ."os.version" ] | del(..|nulls) | join("/") ), ( [ .SchemaV2Manifest.layers[].size ] | add ) ] | join(" ")'
+    echo "**************************"
     docker manifest inspect -v "$1" \
     | jq -c 'if type == "array" then .[] else . end' \
     | jq -r '[ ( .Descriptor.platform | [ .os, .architecture, .variant, ."os.version" ] | del(..|nulls) | join("/") ), ( [ .SchemaV2Manifest.layers[].size ] | add ) ] | join(" ")' \
