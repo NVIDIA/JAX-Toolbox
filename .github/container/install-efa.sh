69c333452d9f9550702babc9a9c836784c7e19bf
Vladislav <vkozlov@nvidia.com>
2024-07-17 16:47:48 -0700

    Add AWS OFI NCCL plugin

--- .github/container/install-efa.sh
+++ .github/container/install-efa.sh
@@ -29,6 +29,27 @@ tar -xf aws-efa-installer-${EFA_INSTALLER_VERSION}.tar.gz && cd aws-efa-installe
 
 popd
 
+# install AWS OFI NCCL plugin
+
+export OFI_PREFIX=/opt/aws-ofi-nccl
+AWS_OFI_NCCL_VERSION=1.9.2-aws
+apt-get install -y libhwloc-dev
+curl -OL https://github.com/aws/aws-ofi-nccl/releases/download/v${AWS_OFI_NCCL_VERSION}/aws-ofi-nccl-${AWS_OFI_NCCL_VERSION}.tar.gz
+tar -xf aws-ofi-nccl-${AWS_OFI_NCCL_VERSION}.tar.gz
+pushd aws-ofi-nccl-${AWS_OFI_NCCL_VERSION}
+./configure --prefix=${OFI_PREFIX} \
+        --with-mpi=/opt/amazon/openmpi \
+        --with-libfabric=/opt/amazon/efa \
+        --with-cuda=/usr/local/cuda \
+        --enable-platform-aws
+make -j $(nproc)
+make install
+popd
+rm -rf aws-ofi-nccl-${AWS_OFI_NCCL_VERSION}
+rm aws-ofi-nccl-${AWS_OFI_NCCL_VERSION}.tar.gz
+echo "PATH=${OFI_PREFIX}/bin:\$PATH" > /etc/profile.d/ofi-aws.sh
+echo "${OFI_PREFIX}/lib" > /etc/ld.so.conf.d/000_ofi_aws.conf
+
 # Clean up
 apt-get clean
 rm -rf /var/lib/apt/lists/*
