b7cfe948974023de78e20b9721409189ab06b97c
Frederic Bastien <fbastien@nvidia.com>
2024-08-19 16:03:36 -0400

    Remove the now removed and enabled by default flag xla_gpu_simplify_all_fp_conversions

--- rosetta/rosetta/projects/vit/scripts/singleprocess_pretrain.sh
+++ rosetta/rosetta/projects/vit/scripts/singleprocess_pretrain.sh
@@ -38,7 +38,7 @@ TRAIN_GPUS=${NUM_GPUS}
 echo "Please make sure ${TRAIN_GPUS} is the number of visible CUDA devices you have"
 
 # Setting XLA flags
-export XLA_FLAGS="--xla_gpu_simplify_all_fp_conversions --xla_gpu_all_reduce_combine_threshold_bytes=136314880 ${XLA_FLAGS}"
+export XLA_FLAGS="--xla_gpu_all_reduce_combine_threshold_bytes=136314880 ${XLA_FLAGS}"
 export XLA_PYTHON_CLIENT_MEM_FRACTION=0.9
 
 # Global batch size
