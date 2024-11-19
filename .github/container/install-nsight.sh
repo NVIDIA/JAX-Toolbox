0c6cde3f0ba064df24617b0765419d809d7bf567
Olli Lupton <olupton@nvidia.com>
2024-11-19 17:19:08 +0100

    disable pull of devtools repo

--- .github/container/install-nsight.sh
+++ .github/container/install-nsight.sh
@@ -6,16 +6,16 @@ UBUNTU_ARCH=$(dpkg --print-architecture)
 UBUNTU_VERSION=$(. /etc/os-release && echo ${ID}${VERSION_ID/./}) # e.g. ubuntu2204
 DEVTOOLS_URL=https://developer.download.nvidia.com/devtools/repos/${UBUNTU_VERSION}/${UBUNTU_ARCH}
 curl -o /usr/share/keyrings/nvidia.pub "${DEVTOOLS_URL}/nvidia.pub"
-echo "deb [signed-by=/usr/share/keyrings/nvidia.pub] ${DEVTOOLS_URL}/ /" > /etc/apt/sources.list.d/devtools-${UBUNTU_VERSION}-${UBUNTU_ARCH}.list
+#echo "deb [signed-by=/usr/share/keyrings/nvidia.pub] ${DEVTOOLS_URL}/ /" > /etc/apt/sources.list.d/devtools-${UBUNTU_VERSION}-${UBUNTU_ARCH}.list
 
 export DEBIAN_FRONTEND=noninteractive
 export TZ=America/Los_Angeles
 
-apt-get update
-apt-get install -y nsight-compute nsight-systems-cli
-apt-get clean
+#apt-get update
+#apt-get install -y nsight-compute nsight-systems-cli
+#apt-get clean
 
-rm -rf /var/lib/apt/lists/*
+#rm -rf /var/lib/apt/lists/*
 
 for NSYS in /opt/nvidia/nsight-systems-cli/2024.5.1 /opt/nvidia/nsight-systems-cli/2024.6.1; do
   if [[ -d "${NSYS}" ]]; then
