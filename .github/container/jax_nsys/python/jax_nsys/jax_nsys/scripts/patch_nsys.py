c03b1053137ff98558f548fe7653bdcb19ed20d4
Olli Lupton <olupton@nvidia.com>
2024-11-19 17:17:56 +0100

    explain what's needed

--- .github/container/jax_nsys/python/jax_nsys/jax_nsys/scripts/patch_nsys.py
+++ .github/container/jax_nsys/python/jax_nsys/jax_nsys/scripts/patch_nsys.py
@@ -1,5 +1,6 @@
 def main():
     """
     Entrypoint for nsys-jax-patch-nsys.
+    TODO: see if the installed nsys's nvtx_gpu_proj_trace recipe needs to be patched to include TID in the output
     """
     pass
