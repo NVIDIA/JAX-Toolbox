2e3073143201f806293468235e50265822cd6efc
Olli Lupton <olupton@nvidia.com>
2024-11-20 13:54:00 +0100

    write the git commit that was installed

--- .github/container/jax_nsys/install.sh
+++ .github/container/jax_nsys/install.sh
@@ -15,23 +15,22 @@ SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 VIRTUALENV="${SCRIPT_DIR}/nsys_jax_venv"
 if [[ ! -d "${VIRTUALENV}" ]]; then
   # Let `virtualenv` find/choose a Python. Currently >=3.10 is supported.
-  virtualenv -p 3.12 -p 3.11 -p 3.10 "$@" "${VIRTUALENV}"
+  virtualenv -p 3.13 -p 3.12 -p 3.11 -p 3.10 "$@" "${VIRTUALENV}"
   . "${VIRTUALENV}/bin/activate"
   python -m pip install -U pip
   # matplotlib is a dependency of Analysis.ipynb
   python -m pip install jupyterlab matplotlib
   # FIXME: install from JAX-Toolbox GitHub? include [jupyter] variant?
   python -m pip install -e "${SCRIPT_DIR}/python/jax_nsys"
-  if ! python -c "import google.protobuf" > /dev/null 2>&1 || ! command -v protoc > /dev/null; then
-    python -m pip install protobuf requests
-    install-protoc "${VIRTUALENV}"
-  fi
+  install-protoc "${VIRTUALENV}"
+  # TODO: include this in the main installation so it can be used in --analysis scripts?
   curl -o "${VIRTUALENV}/bin/flamegraph.pl" https://raw.githubusercontent.com/brendangregg/FlameGraph/master/flamegraph.pl
   chmod 755 "${VIRTUALENV}/bin/flamegraph.pl"
 else
   echo "Virtual environment already exists, not installing anything..."
 fi
 if [ -z ${NSYS_JAX_INSTALL_SKIP_LAUNCH+x} ]; then
+  # TODO: point to jax_nsys/analysis/Analysis.ipynb
   echo "Launching: cd ${SCRIPT_DIR} && ${VIRTUALENV}/bin/python -m jupyterlab Analysis.ipynb"
   cd "${SCRIPT_DIR}" && "${VIRTUALENV}/bin/python" -m jupyterlab Analysis.ipynb
 else
