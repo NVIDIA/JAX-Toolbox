5c4b687b918e6569bca43758c346ad8e67460154
Olli Lupton <olupton@nvidia.com>
2024-12-03 14:47:02 +0100

    nsys-jax: re-work to be more pip-install-able (#1165)
    The overarching goal of this PR is to get closer to a world where the
    `nsys-jax` tooling is straightforwardly `pip install`-able. While the
    diff looks scary, it's mostly re-organisation.
    
    Substantive changes:
    - `nsys-jax` no longer bundles Python code in the output archives, the
    `install.sh` script provided for users to run on local machines becomes,
    loosely, `install 'pip nsys-jax[jupyter] @
    git+https://github.com/NVIDIA/JAX-Toolbox.git@COMMIT#subdirectory=.github/container/nsys_jax'`,
    where `COMMIT` corresponds to the `nsys-jax` command that produced the
    archive. For the `ghcr.io/nvidia/jax` containers, this is the commit of
    JAX-Toolbox that triggered the container build.
    
    Changes included:
    - Introduce `/opt/pip-tools-post-install.d`, which `pip-finalize.sh`
    will execute the contents of *after* installing the `pip`-managed world
    - Migrate `install-protoc` to use this, so `pip-finalize.sh` can forget
    about that detail.
    - Install
    https://github.com/brendangregg/FlameGraph/blob/master/flamegraph.pl via
    this.
    - Patch the `nvtx_gpu_proj_trace` Python code in Nsight Systems 2024.5
    and 2024.6 via this.
    - Move `nsys-jax` installation (specifically for the containers) into
    `install-nsys-jax.sh` and thereby clean up `install-nsight.sh`. The new
    script has to be told the git commit hash of JAX-Toolbox that is being
    built, because `nsys-jax` bakes this into an installation script in its
    output `.zip` archives to ensure the local environment matches the
    profile-collection environment.
    - The CLI tools like `nsys-jax`, `nsys-jax-combine` and `install-protoc`
    are now handled via `[project.scripts]` in `pyproject.toml` instead of
    being standalone Python scripts. This is "more standard", and also makes
    it easier to share code between `nsys-jax` and `nsys-jax-combine`.
    - The Python library is renamed from `jax_nsys` to `nsys_jax` for
    consistency.
    - It's now possible to set the default data loading path via the
    `NSYS_JAX_DEFAULT_PREFIX` environment variable; previously the default
    was the current working directory, but that can be inconvenient to steer
    in Jupyter environments.

---
+++ .github/container/nsys_jax/nsys_jax/scripts/utils.py
@@ -0,0 +1,77 @@
+import contextlib
+import importlib.resources
+import os
+import pathlib
+import shlex
+import subprocess
+import sys
+
+
+def shuffle_analysis_arg(analysis):
+    """
+    Helper for parsing --nsys-jax-analysis[-arg] (nsys-jax) and --analysis[-arg]
+    (nsys-jax-combine) command line options.
+    """
+    if analysis is None:
+        return []
+    # [Script(A), Arg(A1), Arg(A2), Script(B), Arg(B1)] becomes [[A, A1, A2], [B, B1]]
+    out, current = [], []
+    for t, x in analysis:
+        if t == "script":
+            if len(current):
+                out.append(current)
+            current = [x]
+        else:
+            assert t == "arg" and len(current)
+            current.append(x)
+    if len(current):
+        out.append(current)
+    return out
+
+
+def analysis_recipe_path(script):
+    """
+    Return a context manager that yields the path to the analysis script named by
+    `script`. This can either be the name of a bundled analysis script from the
+    the installed analyses/ directory, or a filesystem path.
+    """
+    script_file = importlib.resources.files("nsys_jax").joinpath(
+        "analyses", script + ".py"
+    )
+    if script_file.is_file():
+        return script_file
+    assert os.path.exists(
+        script
+    ), f"{script} does not exist and is not the name of a built-in analysis script"
+    return contextlib.nullcontext(pathlib.Path(script))
+
+
+def execute_analysis_script(
+    *, data: pathlib.Path, script: str, args: list[str]
+) -> tuple[subprocess.CompletedProcess, pathlib.Path]:
+    """
+    Run the analysis script named by `script` on the profile data in the `data`
+    directory (structure the same as nsys-jax[-combine] output archives), saving any
+    output files to subdirectory of `data` named `output_prefix`.
+    """
+    with analysis_recipe_path(script) as script_path:
+        analysis_command = [sys.executable, str(script_path)] + args + [str(data)]
+
+        # Derive a unique name slug from the analysis script name
+        def with_suffix(suffix):
+            return data / "analysis" / (script_path.stem + suffix)
+
+        n, suffix = 1, ""
+        while with_suffix(suffix).exists():
+            suffix = f"-{n}"
+            n += 1
+        working_dir = with_suffix(suffix)
+        working_dir.mkdir(parents=True)
+        print(
+            f"Running analysis script: {shlex.join(analysis_command)} in {working_dir}"
+        )
+        result = subprocess.run(
+            analysis_command,
+            cwd=working_dir,
+        )
+    return result, working_dir.relative_to(data)
