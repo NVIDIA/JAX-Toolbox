77e98f05df6be47de67df8ba142d7116f792c949
Vladislav Kozlov <vkozlov@nvidia.com>
2024-12-20 15:58:01 -0700

    Pin orbax-checkpoint to 0.10.2 and refactor build-jax.sh for JAX-release

--- .github/container/build-jax.sh
+++ .github/container/build-jax.sh
@@ -318,7 +318,11 @@ else
 fi
 
 # install jax and jaxlib
-pip --disable-pip-version-check install -e ${BUILD_PATH_JAXLIB}/jaxlib -e ${BUILD_PATH_JAXLIB}/jax-cuda-pjrt -e ${BUILD_PATH_JAXLIB}/jax-cuda-plugin -e "${SRC_PATH_JAX}"
+pip --disable-pip-version-check install -e ${BUILD_PATH_JAXLIB}/jaxlib -e ${BUILD_PATH_JAXLIB}/jax-cuda-pjrt -e ${BUILD_PATH_JAXLIB}/jax-cuda-plugin
+jaxlib_version=$(pip show jaxlib | grep Version | tr ':' '\n' | tail -1)
+sed -i "s|^_current_jaxlib_version.*|_current_jaxlib_version = '${jaxlib_version}'|" /opt/jax/setup.py
+sed -i "s|      f'jaxlib >={_minimum_jaxlib_version}, <={_jax_version}',|      f'jaxlib>=0.4.30',|" /opt/jax/setup.py
+pip --disable-pip-version-check install -e "${SRC_PATH_JAX}"
 
 ## after installation (example)
 # jax               0.4.36.dev20241125+f828f2d7d /opt/jax
