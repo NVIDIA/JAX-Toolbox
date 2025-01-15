d95d51c7aae13f760e5183b9316e11c027bb07e7
STEFANO BOSISIO <sbosisio@sbosisio-mlt.client.nvidia.com>
2025-01-15 09:36:47 +0000

    fix installs and symlinks

--- .github/container/install-cudnn.sh
+++ .github/container/install-cudnn.sh
@@ -2,46 +2,6 @@
 
 set -ex
 
-export DEBIAN_FRONTEND=noninteractive
-export TZ=America/Los_Angeles
-
-CUDNN_MAJOR_VERSION=9
-
-apt-get update
-
-# Extract major CUDA version from `nvcc --version` output line
-# Input: "Cuda compilation tools, release X.Y, VX.Y.Z"
-# Output: X
-cuda_major_version=$(nvcc --version | sed -n 's/^.*release \([0-9]*\.[0-9]*\).*$/\1/p' | cut -d. -f1)
-
-# Find latest cuDNN version compatible with existing CUDA by matching
-# ${cuda_major_version} in the package version string
-# In most cases cuDNN release is behind CUDA ones. It is considered, that major 
-# version of CUDA and cuDNN are compatible.
-# For example, CUDA 12.3 + cuDNN 8.9.6 (libcudnn8 version: 8.9.6.50-1+cuda12.2) is 
-# considered to be compatible.
-if [[ ${CUDNN_MAJOR_VERSION} -le 8 ]]; then
-    libcudnn_name=libcudnn${CUDNN_MAJOR_VERSION}
-    libcudnn_dev_name=libcudnn${CUDNN_MAJOR_VERSION}-dev
-    version_pattern="s/^Version: \(.*+cuda${cuda_major_version}\.[0-9]*\)$/\1/p"
-elif [[ ${CUDNN_MAJOR_VERSION} -eq 9 ]]; then
-    libcudnn_name=libcudnn${CUDNN_MAJOR_VERSION}-cuda-${cuda_major_version}
-    libcudnn_dev_name=libcudnn${CUDNN_MAJOR_VERSION}-dev-cuda-${cuda_major_version}
-    version_pattern="s/^Version: \(${CUDNN_MAJOR_VERSION}\.[0-9.-]*\)$/\1/p"
-fi
-libcudnn_version=$(apt-cache show $libcudnn_name |  sed -n "$version_pattern" | head -n 1)
-libcudnn_dev_version=$(apt-cache show $libcudnn_dev_name | sed -n "$version_pattern" | head -n 1)
-if [[ -z "${libcudnn_version}" || -z "${libcudnn_dev_version}" ]]; then
-    echo "Could not find compatible cuDNN version for CUDA ${cuda_version}"
-    exit 1
-fi
-
-apt-get install -y \
-    ${libcudnn_name}=${libcudnn_version} \
-    ${libcudnn_dev_name}=${libcudnn_dev_version}
-apt-get clean
-rm -rf /var/lib/apt/lists/*
-
 # Create a prefix with include/ and lib/ directories containing symlinks to the cuDNN
 # version that was just installed; this is useful to pass to XLA to avoid it fetching
 # its own copy of cuDNN.
@@ -50,8 +10,15 @@ if [[ -d "${prefix}" ]]; then
   echo "Skipping link farm creation"
   exit 1
 fi
+
 arch=$(uname -m)-linux-gnu
-for cudnn_file in $(dpkg -L ${libcudnn_name} ${libcudnn_dev_name} | sort -u); do
+libcudnn_pkgs=$(dpkg -l 'libcudnn*' | awk '/^ii/ {print $2}')
+if [[ -z "${libcudnn_pkgs}" ]]; then
+  echo "No libcudnn packages installed."
+  exit 1
+fi
+
+for cudnn_file in $(dpkg -L ${libcudnn_pkgs} | sort -u); do
   # Real files and symlinks are linked into $prefix
   if [[ -f "${cudnn_file}" || -h "${cudnn_file}" ]]; then
     # Replace /usr with $prefix
--- .github/container/install-nccl.sh
+++ .github/container/install-nccl.sh
@@ -2,37 +2,6 @@
 
 set -ex -o pipefail
 
-export DEBIAN_FRONTEND=noninteractive
-export TZ=America/Los_Angeles
-
-# If NCCL is already installed, don't reinstall it. Print a message and exit
-if dpkg -s libnccl2 libnccl-dev &> /dev/null; then
-    echo "NCCL is already installed. Skipping installation."
-else
-    apt-get update
-
-    # Extract CUDA version from `nvcc --version` output line
-    # Input: "Cuda compilation tools, release X.Y, VX.Y.Z"
-    # Output: X.Y
-    cuda_version=$(nvcc --version | sed -n 's/^.*release \([0-9]*\.[0-9]*\).*$/\1/p')
-
-    # Find latest NCCL version compatible with existing CUDA by matching
-    # ${cuda_version} in the package version string
-    libnccl2_version=$(apt-cache show libnccl-dev | sed -n "s/^Version: \(.*+cuda${cuda_version}\)$/\1/p" | head -n 1)
-    libnccl_dev_version=$(apt-cache show libnccl-dev | sed -n "s/^Version: \(.*+cuda${cuda_version}\)$/\1/p" | head -n 1)
-    if [[ -z "${libnccl2_version}" || -z "${libnccl_dev_version}" ]]; then
-        echo "Could not find compatible NCCL version for CUDA ${cuda_version}"
-        exit 1
-    fi
-
-    apt-get install -y \
-        libnccl2=${libnccl2_version} \
-        libnccl-dev=${libnccl_dev_version}
-
-    apt-get clean
-    rm -rf /var/lib/apt/lists/*
-fi
-
 # Create a prefix with include/ and lib/ directories containing symlinks to the NCCL
 # version installed at the system level; this is useful to pass to XLA to avoid it
 # fetching its own copy.
@@ -42,7 +11,15 @@ if [[ -d "${prefix}" ]]; then
   exit 1
 fi
 arch=$(uname -m)-linux-gnu
-for nccl_file in $(dpkg -L libnccl2 libnccl-dev | sort -u); do
+nccl_packages=$(dpkg -l 'libnccl*' | awk '/^ii/ {print $2}')
+
+if [[ -z "${nccl_packages}" ]]; then
+  echo "No NCCL packages installed."
+  exit 1
+fi
+
+
+for nccl_file in $(dpkg -L ${nccl_packages} | sort -u); do
   # Real files and symlinks are linked into $prefix
   if [[ -f "${nccl_file}" || -h "${nccl_file}" ]]; then
     # Replace /usr with $prefix and remove arch-specific lib directories
