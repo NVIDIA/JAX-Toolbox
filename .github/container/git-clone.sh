5048eab9c16846abc402079cb7f251637b1ebc5c
Vladislav <vkozlov@nvidia.com>
2025-01-15 10:51:07 -0800

    Clean up .git for secrets (#1253)

--- .github/container/git-clone.sh
+++ .github/container/git-clone.sh
@@ -79,6 +79,7 @@ COMMIT_SHA=$(git rev-parse HEAD)
 git submodule update --init --recursive
 if [[ "${GIT_REPO}" == *"gitlab"* ]]; then
   git remote remove origin
+  grep -r gitlab-ci-token .git | awk -F: '{print $1}' | xargs rm -f
 fi
 popd
 
