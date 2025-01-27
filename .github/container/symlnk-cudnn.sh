1a138444c98c171a000be945955e64b2b0db0730
Steboss <sbosisio@nvidia.com>
2025-01-27 17:15:36 +0000

    Update the dockerfile base image to cuda-dl-base (#1248)
    Update the base docker image to `cuda-dl-base`
    
    ---------
    
    Co-authored-by: STEFANO BOSISIO <sbosisio@sbosisio-mlt.client.nvidia.com>

---
+++ .github/container/symlnk-cudnn.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -ex
+
+CUDNN_MAJOR_VERSION=9
+
+# Create a prefix with include/ and lib/ directories containing symlinks to the cuDNN
+# version that was just installed; this is useful to pass to XLA to avoid it fetching
+# its own copy of cuDNN.
+prefix=/opt/nvidia/cudnn
+if [[ -d "${prefix}" ]]; then
+  echo "Skipping link farm creation"
+  exit 1
+fi
+
+arch=$(uname -m)-linux-gnu
+libcudnn_pkgs=$(dpkg -l 'libcudnn*' | awk '/^ii/ {print $2}')
+if [[ -z "${libcudnn_pkgs}" ]]; then
+  echo "No libcudnn packages installed."
+  exit 1
+fi
+
+for cudnn_file in $(dpkg -L ${libcudnn_pkgs} | sort -u); do
+  # Real files and symlinks are linked into $prefix
+  if [[ -f "${cudnn_file}" || -h "${cudnn_file}" ]]; then
+    # Replace /usr with $prefix
+    nosysprefix="${cudnn_file#"/usr/"}"
+    # include/x86_64-linux-gpu -> include/
+    noarchinclude="${nosysprefix/#"include/${arch}"/include}"
+    # cudnn_v9.h -> cudnn.h
+    noverheader="${noarchinclude/%"_v${CUDNN_MAJOR_VERSION}.h"/.h}"
+    # lib/x86_64-linux-gnu -> lib/
+    noarchlib="${noverheader/#"lib/${arch}"/lib}"
+    link_name="${prefix}/${noarchlib}"
+    link_dir=$(dirname "${link_name}")
+    mkdir -p "${link_dir}"
+    ln -s "${cudnn_file}" "${link_name}"
+  else
+    echo "Skipping ${cudnn_file}"
+  fi
+done
---
+++ .github/container/symlnk-nccl.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -ex -o pipefail
+
+# Create a prefix with include/ and lib/ directories containing symlinks to the NCCL
+# version installed at the system level; this is useful to pass to XLA to avoid it
+# fetching its own copy.
+prefix=/opt/nvidia/nccl
+if [[ -d "${prefix}" ]]; then
+  echo "Skipping link farm creation"
+  exit 1
+fi
+arch=$(uname -m)-linux-gnu
+nccl_packages=$(dpkg -l 'libnccl*' | awk '/^ii/ {print $2}')
+
+if [[ -z "${nccl_packages}" ]]; then
+  echo "No NCCL packages installed."
+  exit 1
+fi
+
+for nccl_file in $(dpkg -L ${nccl_packages} | sort -u); do
+  # Real files and symlinks are linked into $prefix
+  if [[ -f "${nccl_file}" || -h "${nccl_file}" ]]; then
+    # Replace /usr with $prefix and remove arch-specific lib directories
+    nosysprefix="${nccl_file#"/usr/"}"
+    noarchlib="${nosysprefix/#"lib/${arch}"/lib}"
+    link_name="${prefix}/${noarchlib}"
+    link_dir=$(dirname "${link_name}")
+    mkdir -p "${link_dir}"
+    ln -s "${nccl_file}" "${link_name}"
+  else
+    echo "Skipping ${nccl_file}"
+  fi
+done
