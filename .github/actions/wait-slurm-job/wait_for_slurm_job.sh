27b0dd8a77a75b7d28c8849c66b466d673deeb2e
Steboss <stefanobosisio1@gmail.com>
2025-02-10 16:11:38 +0000

    fix pyxis submit slurm

---
+++ .github/actions/wait-slurm-job/wait_for_slurm_job.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# waits for a SLURM job to complete, fail, or be cancelled.
+function wait_for_slurm_job() {
+
+    opts=$(set +o)
+    set +x
+
+    host=$1
+    job_id=$2
+    check_every=${3:-15}
+
+    while true; do
+        status=$(ssh $host squeue --job $job_id --noheader --format=%T 2>/dev/null || echo "SSH error: $?")
+        echo "[$(date)] job $job_id: $status"
+
+        if [ -z "$status" ]; then
+            break
+        fi
+
+        sleep $check_every
+    done
+
+    eval "$opts"
+}
