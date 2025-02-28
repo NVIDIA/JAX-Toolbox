4e7b1b474cb4a2f4e900df5e680c52580f522ceb
Olli Lupton <olupton@nvidia.com>
2025-02-28 16:55:22 +0100

    nsys: add patch for 2024.6.2, expand testing (#1323)
    - Expand `nsys-jax` test coverage.
    - Expand `nsys-jax-patch-nsys` coverage to Nsight Systems 2024.6.2

--- .github/container/nsys_jax/tests/test_example_program.py
+++ .github/container/nsys_jax/tests/test_example_program.py
@@ -1,49 +1,24 @@
-from nsys_jax import (
-    ensure_compiled_protos_are_importable,
-    load_profiler_data,
-)
+from nsys_jax import load_profiler_data
 import os
 import pathlib
 import pytest  # type: ignore
 import sys
-import tempfile
-import zipfile
 
 helper_dir = os.path.join(os.path.dirname(__file__), "nsys_jax_test_helpers")
 if helper_dir not in sys.path:
     sys.path.insert(0, helper_dir)
-from nsys_jax_test_helpers import nsys_jax  # noqa: E402
+from nsys_jax_test_helpers import nsys_jax_archive  # noqa: E402
 
 
 @pytest.fixture(scope="module")
-def example_program():
+def profiler_data():
     """
-    Fixture that yields an extracted archive of the result of profiling
-    example_program.py with nsys-jax.
+    Fixture that yields the loaded result of profiling example_program.py with nsys-jax.
     """
-    tmpdir = tempfile.TemporaryDirectory()
-    archive = nsys_jax(
+    outdir = nsys_jax_archive(
         [sys.executable, os.path.join(os.path.dirname(__file__), "example_program.py")]
     )
-    old_dir = os.getcwd()
-    os.chdir(tmpdir.name)
-    try:
-        with zipfile.ZipFile(archive) as zf:
-            zf.extractall()
-    finally:
-        os.chdir(old_dir)
-    # Make sure the protobuf bindings can be imported, the generated .py will go into
-    # a temporary directory that is not currently cleaned up. The bindings cannot be
-    # un-imported from the test process, so there is a tacit assumption that in a given
-    # test session there will only be one set of .proto files and it doesn't matter
-    # which ones are picked up.
-    ensure_compiled_protos_are_importable(prefix=pathlib.Path(tmpdir.name))
-    return tmpdir
-
-
-@pytest.fixture(scope="module")
-def profiler_data(example_program):
-    return load_profiler_data(pathlib.Path(example_program.name))
+    return load_profiler_data(pathlib.Path(outdir.name))
 
 
 def test_comms(profiler_data):
