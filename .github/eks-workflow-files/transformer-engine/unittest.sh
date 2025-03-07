6ce524746af09317de843c8d899a935916595dda
Alex Y. Chan <alechan@nvidia.com>
2025-03-07 12:11:36 +0000

    Consolidate te job manifests

--- .github/eks-workflow-files/transformer-engine/unittest.sh
+++ .github/eks-workflow-files/transformer-engine/unittest.sh
@@ -1,74 +1,20 @@
-apiVersion: batch/v1
-kind: Job
-metadata:
-  labels:
-    kueue.x-k8s.io/queue-name: p5-queue
-  name: JOB_NAME
-spec:
-  activeDeadlineSeconds: 1200
-  template:
-    spec:
-      containers:
-      - command:
-        - bash
-        - -xo
-        - pipefail
-        - -c
-        - |
-          pip install pytest-reportlog pytest-xdist
+#!/bin/bash
+pip install pytest-reportlog pytest-xdist
 
-          # Start MPS daemon
-          nvidia-cuda-mps-control -d
+# Start MPS daemon
+nvidia-cuda-mps-control -d
 
-          # TE's default is slightly different, without the hyphen
-          export TE_PATH=${SRC_PATH_TRANSFORMER_ENGINE}
+# TE's default is slightly different, without the hyphen
+export TE_PATH=${SRC_PATH_TRANSFORMER_ENGINE}
 
-          # 1 GPU per worker, 6 workers per GPU
-          pytest-xdist.sh 1 6 pytest-report-L0-unittest.jsonl bash ${TE_PATH}/qa/L0_jax_unittest/test.sh | tee -a ${LOG_DIR}/tests.log
+# 1 GPU per worker, 6 workers per GPU
+pytest-xdist.sh 1 6 pytest-report-L0-unittest.jsonl bash ${TE_PATH}/qa/L0_jax_unittest/test.sh | tee -a ${LOG_DIR}/tests.log
 
-          # 8 GPUs per worker, 1 worker per GPU. pytest-xdist.sh allows aggregation
-          # into a single .jsonl file of results from multiple pytest invocations
-          # inside the test.sh script, so it's useful even with a single worker per
-          # device.
-          pytest-xdist.sh 8 1 pytest-report-L0-distributed-unittest.jsonl bash ${TE_PATH}/qa/L0_jax_distributed_unittest/test.sh | tee -a ${LOG_DIR}/tests.log
-          pytest-xdist.sh 8 1 pytest-report-L1-distributed-unittest.jsonl bash ${TE_PATH}/qa/L1_jax_distributed_unittest/test.sh | tee -a ${LOG_DIR}/tests.log
+# 8 GPUs per worker, 1 worker per GPU. pytest-xdist.sh allows aggregation
+# into a single .jsonl file of results from multiple pytest invocations
+# inside the test.sh script, so it's useful even with a single worker per
+# device.
+pytest-xdist.sh 8 1 pytest-report-L0-distributed-unittest.jsonl bash ${TE_PATH}/qa/L0_jax_distributed_unittest/test.sh | tee -a ${LOG_DIR}/tests.log
+pytest-xdist.sh 8 1 pytest-report-L1-distributed-unittest.jsonl bash ${TE_PATH}/qa/L1_jax_distributed_unittest/test.sh | tee -a ${LOG_DIR}/tests.log
 
-          touch /opt/output/done
-        env:
-        - name: LOG_DIR
-          value: /opt/output
-        image: IMAGE_URI
-        name: transformer-engine
-        resources:
-          limits:
-            nvidia.com/gpu: 8
-        volumeMounts:
-        - mountPath: /opt/output
-          name: output
-      - command:
-        - sh
-        - -c
-        - |
-          while [ ! -f /opt/output/done ]; do
-            sleep 5
-          done
-
-          # Upload logs to S3 bucket
-          aws s3 cp /opt/output/ s3://${S3_BUCKET}/${CI_NAME}/${RUN_ID}/ --recursive --exclude "*" --include "*.log"
-        env:
-        - name: RUN_ID
-          value: JOB_NAME
-        - name: S3_BUCKET
-          value: jax-toolbox-eks-output
-        - name: CI_NAME
-          value: transformer-engine
-        image: amazon/aws-cli
-        name: upload-logs
-        volumeMounts:
-        - mountPath: /opt/output
-          name: output
-      imagePullSecrets:
-      - name: SECRETS_NAME
-      restartPolicy: Never
-      volumes:
-      - name: output
+touch /opt/output/done
