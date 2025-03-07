996a0237dbff0c969ff1520c7f3b02ba3fcd198d
Alex Y. Chan <alechan@nvidia.com>
2025-03-07 12:46:54 +0000

    Add mutli gpu script

---
+++ .github/eks-workflow-files/transformer-engine/multi-gpu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+nvidia-smi
+pip install pytest \
+      pytest-reportlog \
+      cuda-python \
+      -r ${SRC_PATH_TRANSFORMER_ENGINE}/examples/jax/encoder/requirements.txt
+pip install pytest-reportlog pytest-xdist
+
+set -ex
+cd ${SRC_PATH_TRANSFORMER_ENGINE}/examples/jax/encoder
+pytest --report-log=/output/pytest-report.jsonl \
+test_single_gpu_encoder.py \
+test_multigpu_encoder.py \
+test_model_parallel_encoder.py | tee -a ${LOG_DIR}/tests.log
+
+touch /opt/output/done
