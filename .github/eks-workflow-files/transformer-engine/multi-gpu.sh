0bec3bd5c1c89e3a238c4e170f9dc5b63eb0208e
Alex Y. Chan <alechan@nvidia.com>
2025-03-07 13:10:29 +0000

    Fix script paste indentation issues

--- .github/eks-workflow-files/transformer-engine/multi-gpu.sh
+++ .github/eks-workflow-files/transformer-engine/multi-gpu.sh
@@ -1,15 +1,15 @@
-        nvidia-smi
-        pip install pytest \
-              pytest-reportlog \
-              cuda-python \
-              -r ${SRC_PATH_TRANSFORMER_ENGINE}/examples/jax/encoder/requirements.txt
-        pip install pytest-reportlog pytest-xdist
-        
-        set -ex
-        cd ${SRC_PATH_TRANSFORMER_ENGINE}/examples/jax/encoder
-        pytest --report-log=/output/pytest-report.jsonl \
-        test_single_gpu_encoder.py \
-        test_multigpu_encoder.py \
-        test_model_parallel_encoder.py | tee -a ${LOG_DIR}/tests.log
-        
-        touch /opt/output/done
+          nvidia-smi
+          pip install pytest \
+                pytest-reportlog \
+                cuda-python \
+                -r ${SRC_PATH_TRANSFORMER_ENGINE}/examples/jax/encoder/requirements.txt
+          pip install pytest-reportlog pytest-xdist
+          
+          set -ex
+          cd ${SRC_PATH_TRANSFORMER_ENGINE}/examples/jax/encoder
+          pytest --report-log=/output/pytest-report.jsonl \
+          test_single_gpu_encoder.py \
+          test_multigpu_encoder.py \
+          test_model_parallel_encoder.py | tee -a ${LOG_DIR}/tests.log
+          
+          touch /opt/output/done
