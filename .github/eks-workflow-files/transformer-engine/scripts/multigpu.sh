9c3d392aa73fa054e0eb099ca9da9d8eac9abe2f
Alex Y. Chan <alechan@nvidia.com>
2025-03-11 11:18:39 +0000

    Update log output path from env in EKS jobs

--- .github/eks-workflow-files/transformer-engine/scripts/multigpu.sh
+++ .github/eks-workflow-files/transformer-engine/scripts/multigpu.sh
@@ -7,9 +7,9 @@
           
           set -ex
           cd ${SRC_PATH_TRANSFORMER_ENGINE}/examples/jax/encoder
-          pytest --report-log=/output/pytest-report.jsonl \
+          pytest --report-log=${LOG_DIR}/pytest-report.jsonl \
           test_single_gpu_encoder.py \
           test_multigpu_encoder.py \
           test_model_parallel_encoder.py | tee -a ${LOG_DIR}/tests.log
           
-          touch /opt/output/done
+          touch ${LOG_DIR}/done
