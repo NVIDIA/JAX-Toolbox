58954fad75b1da17999e90cbbf9a6261286a765f
Alex Y. Chan <alechan@nvidia.com>
2025-05-21 15:41:06 +0000

    Add GPU set partition option in distributed pytest

--- .github/container/pytest-xdist.sh
+++ .github/container/pytest-xdist.sh
@@ -7,9 +7,55 @@
 # pytest-xdist.sh 1 10 1gpu-logfile.jsonl tests/single_gpu
 # pytest-xdist.sh 2 5 2gpu-logfile.jsonl tests/two_gpu
 set -e
+
+# Parse named args.
+while [[ "$1" == --* ]]; do
+case $1 in
+  --*=*)
+      pair=($(echo $1 | awk -F'=' '{ print $1, $2 }'))
+      key=${pair[0]#--}
+      value=${pair[1]}
+      declare "$key=$value"
+      ;;
+esac
+shift
+done
+
+# Set partition of GPUs by index from START_GPU_IDX to END_GPU_IDX to use in tests
+# By default, use all detected GPUs - override this by providing named arguments for GPU indexes
+#
+# # Use a partition of GPUs 0,1,2,3 for tests
+# pytest-xdist.sh --START_GPU_IDX=0 --END_GPU_IDX=3 4 1 3gpu-logfile.jsonl bash tests/four_gpu
+if [[ ! -n "$START_GPU_IDX" && -n "$END_GPU_IDX" ]] || [[ ! -z "$START_GPU_IDX" && -z "$END_GPU_IDX" ]]; then
+    echo "Must set both START_GPU_IDX and END_GPU_IDX or neither"
+    exit 1
+fi
+if [ -z ${START_GPU_IDX+x} ] && [ -z ${END_GPU_IDX+x} ] ; then
+    # default, use all detected GPUs
+    START_GPU_IDX=$(nvidia-smi -L | awk  '{ print $2 }' | tr -d ':' | sort | head -n 1)
+    END_GPU_IDX=$(nvidia-smi -L | awk  '{ print $2 }' | tr -d ':' | sort -rn | head -n 1)
+fi
+
+# Check requested GPU partition is valid
+PARTITION_GPU_IDXS=$(seq -s , $START_GPU_IDX $END_GPU_IDX)
+ALL_GPU_IDXS=$(nvidia-smi -L | awk '{ print $2 }' | tr -d ':' | paste -s -d,)
+if [[ ! $ALL_GPU_IDXS =~ $PARTITION_GPU_IDXS ]]; then
+   echo "Requested partition GPU index sequence \"$PARTITION_GPU_IDXS\" is not valid"
+   exit 1
+fi
+
+# How many GPUs are attached to the system
+GPU_COUNT=$((END_GPU_IDX - START_GPU_IDX + 1))
+export NUM_GPUS=$GPU_COUNT # variable used in $TE_PATH/examples/jax/encoder/run_test_multiprocessing_encoder.sh
+
 # How many GPUs will be visible to each worker process.
 GPUS_PER_WORKER=$1
 shift
+if [[ ! $GPUS_PER_WORKER -le $GPU_COUNT ]]; then
+   echo "Requested GPUs per worker ($GPUS_PER_WORKER) is greater than GPUs available in partition ($GPU_COUNT)"
+   exit 1
+fi
+
 # How many workers will submit work to each GPU. If this is more than 1 then MPS should
 # be used to allow fine-grained interleaving of work submitted from different processes
 WORKERS_PER_GPU=$1
@@ -17,13 +63,11 @@ shift
 # pytest-reportlog logfile
 LOGFILE=$1
 shift
-# How many GPUs are attached to the system
-GPU_COUNT=$(nvidia-smi -L | wc -l)
 # Create a FIFO that multiple child pytest invocations (nested inside $@) can write to
 FIFO=$(mktemp -u)
 PYTEST_ADDOPTS+=" --report-log=${FIFO} --dist=load"
 for worker in $(seq 0 $((WORKERS_PER_GPU-1))); do
-  for first_device in $(seq 0 $GPUS_PER_WORKER $((GPU_COUNT-1))); do
+  for first_device in $(seq $START_GPU_IDX $GPUS_PER_WORKER $((START_GPU_IDX + GPU_COUNT-1))); do
     last_device=$((first_device+GPUS_PER_WORKER-1))
     devices=$(seq -s , $first_device $last_device)
     device_range="${first_device}"
