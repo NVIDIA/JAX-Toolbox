3f6086053d268e8a0f45d4f5206293483cfc4283
Vladislav Kozlov <vkozlov@nvidia.com>
2025-05-22 10:14:59 -0700

    JTB to 25.05.1-devel-from-april-21: don't install jax in build-jax.sh

--- .github/container/build-jax.sh
+++ .github/container/build-jax.sh
@@ -326,11 +326,11 @@ fi
 pip uninstall -y jax jaxlib jax-cuda${TF_CUDA_MAJOR_VERSION}-pjrt jax-cuda${TF_CUDA_MAJOR_VERSION}-plugin
 
 # install jax and jaxlib
-pip --disable-pip-version-check install -e ${BUILD_PATH_JAXLIB}/jaxlib -e ${BUILD_PATH_JAXLIB}/jax_cuda${TF_CUDA_MAJOR_VERSION}_pjrt -e ${BUILD_PATH_JAXLIB}/jax_cuda${TF_CUDA_MAJOR_VERSION}_plugin 
+# pip --disable-pip-version-check install -e ${BUILD_PATH_JAXLIB}/jaxlib -e ${BUILD_PATH_JAXLIB}/jax_cuda${TF_CUDA_MAJOR_VERSION}_pjrt -e ${BUILD_PATH_JAXLIB}/jax_cuda${TF_CUDA_MAJOR_VERSION}_plugin 
 jaxlib_version=$(pip show jaxlib | grep Version | tr ':' '\n' | tail -1)
 sed -i "s|^_current_jaxlib_version.*|_current_jaxlib_version = '${jaxlib_version}'|" /opt/jax/setup.py
 sed -i "s|      f'jaxlib >={_minimum_jaxlib_version}, <={_jax_version}',|      f'jaxlib>=0.5.0',|" /opt/jax/setup.py
-pip --disable-pip-version-check install -e ${BUILD_PATH_JAXLIB}/jax
+# pip --disable-pip-version-check install -e ${BUILD_PATH_JAXLIB}/jax
 
 ## after installation (example)
 # jax                     0.5.4.dev20250325    /opt/jaxlibs/jax
