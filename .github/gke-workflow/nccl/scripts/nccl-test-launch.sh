32e78b9d0cce773942258ef371a79fb1edef8a59
Alex Y. Chan <alechan@nvidia.com>
2025-06-13 09:16:22 +0000

    Set nonzero node rank nccl test behaviour

--- .github/gke-workflow/nccl/scripts/nccl-test-launch.sh
+++ .github/gke-workflow/nccl/scripts/nccl-test-launch.sh
@@ -8,6 +8,12 @@ pushd /scripts;
 /scripts/generate_hostfiles.sh ${@};
 popd;
 
+COMPLETION_FLAG=/opt/output/$BENCHMARK_done
+
 if [ $NODE_RANK = 0 ] ; then
   BENCHMARK=$BENCHMARK NHOSTS=$NHOSTS /scripts/test.sh
+  touch $COMPLETION_FLAG
+else
+  while [ ! -f $COMPLETION_FLAG ]; do sleep 10; done
 fi
+
