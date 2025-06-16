b8a121a0ad94ed19db71809a4e9348f14ecd4a24
Olli Lupton <olupton@nvidia.com>
2025-06-16 20:03:13 +0200

    nvshmem: install on base container (#1512)
    In container builds, we want to handle CUDA, cuDNN, NCCL and NVSHMEM at
    the container level.
    The first three of these were already covered, NVSHMEM was not.
    
    See:
    https://github.com/openxla/xla/blob/7787809e48da6df12f8adfba81ee43d18bc60ec8/docs/hermetic_cuda.md#pointing-to-cudacudnnncclnvshmem-redistributions-on-local-file-system
    as modified in https://github.com/openxla/xla/pull/27289

---
+++ .github/container/install-nvshmem.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -exuo pipefail
+
+if [[ -z "${NVSHMEM_VERSION}" ]]; then
+  echo "NVSHMEM_VERSION not set; do not know what to install; aborting..."
+  exit 1
+fi
+
+# Repository for NVSHMEM
+UBUNTU_ARCH=$(dpkg --print-architecture)
+if [[ "${UBUNTU_ARCH}" == "arm64" ]]; then
+  # nvshmem is only published for sbsa, not arm64
+  REPO_ARCH="sbsa"
+elif [[ "${UBUNTU_ARCH}" == "amd64" ]]; then
+  REPO_ARCH="x86_64"
+else
+  echo "Do not know how to map ${UBUNTU_ARCH} onto a compute/cuda repository URL"
+  exit 1
+fi
+UBUNTU_VERSION=$(. /etc/os-release && echo ${ID}${VERSION_ID/./}) # e.g. ubuntu2204
+curl -o /tmp/keyring.deb "https://developer.download.nvidia.com/compute/cuda/repos/${UBUNTU_VERSION}/${REPO_ARCH}/cuda-keyring_1.1-1_all.deb"
+dpkg -i /tmp/keyring.deb
+
+export DEBIAN_FRONTEND=noninteractive
+export TZ=America/Los_Angeles
+
+apt-get update
+apt-get install -y nvshmem=${NVSHMEM_VERSION}*
+apt-get clean
+
+rm -rf /var/lib/apt/lists/* /tmp/keyring.deb
