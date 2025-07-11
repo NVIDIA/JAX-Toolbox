535aedb86f9747d644934044e7e70dfc2dd0a62e
Steboss <stefanobosisio1@gmail.com>
2025-07-11 16:29:51 +0100

    fix CI

--- .github/triage/jax_toolbox_triage/args.py
+++ .github/triage/jax_toolbox_triage/args.py
@@ -282,7 +282,9 @@ def parse_args(args=None) -> argparse.Namespace:
     if args.container_runtime == "local":
         assert (
             args.passing_versions is not None and args.failing_versions is not None
-        ), "For local runtime, --passing-versions and --failing-versions must be provided."
+        ), (
+            "For local runtime, --passing-versions and --failing-versions must be provided."
+        )
         assert (
             args.container is None
             and args.start_date is None
@@ -327,7 +329,7 @@ def parse_args(args=None) -> argparse.Namespace:
     else:
         # None of --{passing,failing}-{versions,container} were passed, make sure the
         # compulsory arguments for the container-level search were passed
-        assert (
-            args.container is not None
-        ), "--container must be passed for the container-level search"
+        assert args.container is not None, (
+            "--container must be passed for the container-level search"
+        )
     return args
