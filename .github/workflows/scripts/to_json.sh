a41f81f44d9356bead7f583b6e5136877b631020
Alex Y. Chan <alechan@nvidia.com>
2025-07-25 10:39:01 +0100

    Add badge label to sitrep

--- .github/workflows/scripts/to_json.sh
+++ .github/workflows/scripts/to_json.sh
@@ -14,3 +14,21 @@ function to_json() {
 
   eval $CMD
 }
+
+XPK_EXIT_CODE=1
+summary="GKE-neor"
+outcome=success
+badge_color=brightgreen
+
+if [ "${XPK_EXIT_CODE}" -gt 0 ]; then
+    badge_color=red
+    outcome=failed
+    summary+=": fail"
+else
+    summary+=": pass"
+fi
+to_json summary \
+        badge_color \
+        outcome | \
+tee sitrep.json
+cat sitrep.json
