d8ba65538ce55d1654409a1f8e7927daa6f552c6
Brian Yang <jinxiny@nvidia.com>
2025-09-25 09:22:19 -0700

    Change of method, use pre-generated requirements.txt

--- .github/container/pip-finalize.sh
+++ .github/container/pip-finalize.sh
@@ -4,83 +4,64 @@ set -eoux pipefail
 
 pushd /opt/pip-tools.d
 
-if [[ -f "requirements-pinned.in" ]]; then
-  # Filter out editable installs and local wheels from requirements-pinned.in
-  grep -v "\-e" requirements-pinned.in | grep -v "sha256=" > requirements-pinned.in.tmp
-  mv requirements-pinned.in.tmp requirements-pinned.in
-  # Extract already-pinned VCS dependencies
-  grep '^[^#].* @ git\+.*@' requirements-pinned.in > requirements.pre-pinned-vcs || true
+# If requirements-pinned.txt exists, skip compilation
+if [[ -f "requirements-pinned.txt" ]]; then
+  cp requirements-pinned.txt requirements.txt
 else
-  touch requirements.pre-pinned-vcs
-fi
+  # First pip-compile gathers all reqs, but we are care only about VCS installs
+  # It's possible there are 2nd degree transitive dependencies that are VCS, so
+  # this is more robust to gather VCS requirements at the cost of pip-compiling
+  # twice
+  pip-compile -o requirements.pre $(ls requirements-*.in)
 
-# First pip-compile gathers all reqs, but we are care only about VCS installs
-# It's possible there are 2nd degree transitive dependencies that are VCS, so
-# this is more robust to gather VCS requirements at the cost of pip-compiling
-# twice
-# Exclude requirements-pinned.in from initial compilation to avoid conflicts
-pip-compile -o requirements.pre $(ls requirements-*.in | grep -v requirements-pinned.in)
+  IFS=$'\n'
+  for line in $(cat requirements.pre | egrep '^[^#].+ @ git\+' || true); do
+    # VCS installs are of the form "PACKAGE @ git+..."
+    PACKAGE=$(echo "$line" | awk '{print $1}')
+    ref=$(yq e ".${PACKAGE}.latest_verified_commit" ${MANIFEST_FILE})
+    if [[ "$line" == *"#subdirectory="* ]]; then
+      # This is required b/c git-refs/commits cannot come after
+      # the subdirectory fragment.
+      # An example of an install that is of this form is:
+      # 'orbax-checkpoint @ git+https://github.com/google/orbax/#subdirectory=checkpoint'
+      echo "${line}" | sed "s/#subdirectory=/@${ref}#subdirectory=/"
+    else
+      echo "${line}@${ref}"
+    fi
+  done | tee requirements.vcs
+  unset IFS
 
-IFS=$'\n'
-for line in $(cat requirements.pre | egrep '^[^#].+ @ git\+' || true); do
-  # VCS installs are of the form "PACKAGE @ git+..."
-  PACKAGE=$(echo "$line" | awk '{print $1}')
-  
-  # Check if this package is already pinned in requirements-pinned.in
-  if grep -q "^${PACKAGE} @ git\+.*@" requirements.pre-pinned-vcs; then
-    # Skipping ${PACKAGE} - already pinned in requirements-pinned.in
-    continue
+  # Second pip-compile includes one more requirements file that pins all vcs installs
+  # Uses a special env var to let our custom pip impl know to treat the following as
+  # equivalent:
+  #
+  # fiddle @ git+https://github.com/google/fiddle
+  # fiddle @ git+https://github.com/google/fiddle@cd4497e4c09bdf95dcccaa1e138c2c125d32d39f
+  #
+  # JAX_TOOLBOX_VCS_EQUIVALENCY is an environment variable enabling custom logic in pip
+  # that treats the above as equivalent and prefers the URI wit the SHA
+  JAX_TOOLBOX_VCS_EQUIVALENCY=true pip-compile -o requirements.txt requirements.vcs $(ls requirements-*.in)
+
+  # If there are unpinned VCS dependencies, error since these should be included in the manifest
+  unpinned_vcs_dependencies=$(cat requirements.txt | egrep '^[^#].+ @ git\+' | egrep -v '^[^#].+ @ git\+.+@' || true)
+  if [[ $(echo -n "$unpinned_vcs_dependencies" | wc -l) -gt 0 ]]; then
+    echo "Unpinned VCS installs found in $(readlink -f requirements.txt):"
+    echo "$unpinned_vcs_dependencies"
+    exit 1
   fi
-  
-  ref=$(yq e ".${PACKAGE}.latest_verified_commit" ${MANIFEST_FILE})
-  if [[ "$line" == *"#subdirectory="* ]]; then
-    # This is required b/c git-refs/commits cannot come after
-    # the subdirectory fragment.
-    # An example of an install that is of this form is:
-    # 'orbax-checkpoint @ git+https://github.com/google/orbax/#subdirectory=checkpoint'
-    echo "${line}" | sed "s/#subdirectory=/@${ref}#subdirectory=/"
+
+  # Replace any tensorflow==X with tensorflow-cpu==X in requirements.txt only on amd64
+  if [[ "$(uname -m)" = "x86_64" ]]; then
+    sed -i 's/^tensorflow==\([0-9.*]\+\)$/tensorflow-cpu==\1/' requirements.txt
   else
-    echo "${line}@${ref}"
+    echo "Skipping TF on $(uname -m)"
   fi
-done | tee requirements.vcs
-unset IFS
-
-# Append pre-pinned VCS dependencies to requirements.vcs
-if [[ -s requirements.pre-pinned-vcs ]]; then
-  # Adding pre-pinned VCS dependencies from requirements-pinned.in
-  cat requirements.pre-pinned-vcs >> requirements.vcs
 fi
 
-# Second pip-compile includes one more requirements file that pins all vcs installs
-# Uses a special env var to let our custom pip impl know to treat the following as
-# equivalent:
-#
-# fiddle @ git+https://github.com/google/fiddle
-# fiddle @ git+https://github.com/google/fiddle@cd4497e4c09bdf95dcccaa1e138c2c125d32d39f
-#
-# JAX_TOOLBOX_VCS_EQUIVALENCY is an environment variable enabling custom logic in pip
-# that treats the above as equivalent and prefers the URI wit the SHA
-JAX_TOOLBOX_VCS_EQUIVALENCY=true pip-compile -o requirements.txt requirements.vcs $(ls requirements-*.in)
-
-# If there are unpinned VCS dependencies, error since these should be included in the manifest
-unpinned_vcs_dependencies=$(cat requirements.txt | egrep '^[^#].+ @ git\+' | egrep -v '^[^#].+ @ git\+.+@' || true)
-if [[ $(echo -n "$unpinned_vcs_dependencies" | wc -l) -gt 0 ]]; then
-  echo "Unpinned VCS installs found in $(readlink -f requirements.txt):"
-  echo "$unpinned_vcs_dependencies"
-  exit 1
-fi
-
-# Replace any tensorflow==X with tensorflow-cpu==X in requirements.txt only on amd64
-if [ "$(uname -m)" = "x86_64" ]; then
-  sed -i 's/^tensorflow==\([0-9.*]\+\)$/tensorflow-cpu==\1/' requirements.txt
-else
-  echo "Skipping TF on $(uname -m)"
-fi
 # --no-deps is required since conflicts can still appear during pip-sync
 pip-sync --pip-args '--no-deps --src /opt' requirements.txt
 
 rm -rf ~/.cache/*
-rm -f requirements.pre-pinned-vcs
 
 # Execute post-install hooks
 for post_install in $(ls /opt/pip-tools-post-install.d/*); do
