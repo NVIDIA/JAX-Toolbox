fd178658b5e66d0485ac705296da3fca102e01e1
Yu-Hang Tang <Tang.Maxin@gmail.com>
2025-11-11 08:59:53 +0000

    merge latest git-clone.sh

--- .github/container/git-clone.sh
+++ .github/container/git-clone.sh
@@ -48,7 +48,7 @@ while [ : ]; do
         ;;
     --)
         shift;
-        break 
+        break
         ;;
   esac
 done
@@ -73,7 +73,7 @@ fi
 
 ## check out the source
 GIT_REPO=$(cut -d# -f1 <<< $GIT_URLREF)
-GIT_REF=$(cut -d# -f2- <<< $GIT_URLREF)
+GIT_REF=$(cut -s -d# -f2- <<< $GIT_URLREF)
 
 echo "Fetching $GIT_REPO#$GIT_REF to $DESTINATION"
 
@@ -86,16 +86,24 @@ if [[ -n "${SPARSE_PATH}" ]]; then
     git sparse-checkout set ${SPARSE_PATH}
     popd
 else
-    git clone ${GIT_REPO} ${DESTINATION}
+git clone ${GIT_REPO} ${DESTINATION}
 fi
 pushd ${DESTINATION}
-git checkout ${GIT_REF}
+if [[ -n "${GIT_REF}" ]]; then
+    git checkout ${GIT_REF}
+fi
 COMMIT_SHA=$(git rev-parse HEAD)
 git submodule update --init --recursive
+if [[ "${GIT_REPO}" == *"gitlab"* ]]; then
+  git remote remove origin
+  if grep -q -r gitlab-ci-token .git; then
+    grep -r gitlab-ci-token .git | awk -F: '{print $1}' | xargs rm -f
+  fi
+  git branch -D main
+fi
 popd
 
 ## update the manifest file
-
 mkdir -p $(dirname ${MANIFEST})
 touch ${MANIFEST}
 PACKAGE=$(basename "${DESTINATION}")
