4a41013494532da4e8da24e6a0ea50cd142c8f05
Alex Y. Chan <alechan@nvidia.com>
2026-01-13 17:04:45 +0000

    Remove test files

--- .github/gke-workflow/jax-vllm-offloading/deploy-transfer.sh
+++
@@ -1,7 +0,0 @@
-kubectl apply -f transfer/deployment/gateway-pod.yml
-kubectl apply -f transfer/deployment/gateway-svc.yml
-
-kubectl apply -f huggingface-secret.yml
-
-kubectl apply -f transfer/deployment/rollout.yml
-kubectl apply -f transfer/deployment/trainer.yml
