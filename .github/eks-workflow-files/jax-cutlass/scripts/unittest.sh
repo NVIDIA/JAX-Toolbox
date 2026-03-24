9f6eadcc43ff014b1957f796f0e5a93689eaab3b
Michael Goldfarb <mgoldfarb@nvidia.com>
2026-03-24 16:01:21 +0000

    generate test report to keep CI workflow happy even through we don't use pytest.

--- .github/eks-workflow-files/jax-cutlass/scripts/unittest.sh
+++ .github/eks-workflow-files/jax-cutlass/scripts/unittest.sh
@@ -1,26 +1,27 @@
-          set -xu -o pipefail
+         set -xu -o pipefail
 
-          LOG_DIR=${LOG_DIR:-/opt/output}
+         LOG_DIR=${LOG_DIR:-/opt/output}
 
-          SRC_ROOT=${SRC_ROOT:-$PWD/jax-cutlass-src}
-          SRC_ROOT=$(realpath $SRC_ROOT)
+         SRC_ROOT=${SRC_ROOT:-$PWD/jax-cutlass-src}
+         SRC_ROOT=$(realpath $SRC_ROOT)
 
-          # Clone CUTLASS examples
-          CUTLASS_ROOT="${SRC_ROOT}/cutlass"
-          CUTLASS_EXAMPLES_ROOT="${CUTLASS_ROOT}/examples/python/CuTeDSL"
-          git clone https://github.com/NVIDIA/cutlass.git ${CUTLASS_ROOT}
+         # Clone CUTLASS examples
+         CUTLASS_ROOT="${SRC_ROOT}/cutlass"
+         CUTLASS_EXAMPLES_ROOT="${CUTLASS_ROOT}/examples/python/CuTeDSL"
+         git clone https://github.com/NVIDIA/cutlass.git ${CUTLASS_ROOT}
 
-          NGPUS=$(nvidia-smi --list-gpus | wc -l)
+         NGPUS=$(nvidia-smi --list-gpus | wc -l)
 
-          # Start MPS daemon
-          nvidia-cuda-mps-control -d
+         # Start MPS daemon
+         nvidia-cuda-mps-control -d
 
-          export PYTHONPATH=${CUTLASS_EXAMPLES_ROOT}
+         export PYTHONPATH=${CUTLASS_EXAMPLES_ROOT}
 
-          # Run the examples
-		  for f in ${CUTLASS_ROOT}/examples/python/CuTeDSL/jax/*.py; do
-		    echo "[Executing] $f"
-		  	python $f
-		  done
+         # Run the examples
+         for f in ${CUTLASS_ROOT}/examples/python/CuTeDSL/jax/*.py; do
+             echo "[Executing] $f"
+             python $f && outcome="passed" || outcome="failed"
+             echo "{\"outcome\": \"${outcome}\", \"nodeid\": \"${f}\"}" >> ${LOG_DIR}/pytest-report.jsonl
+         done
 
-          touch ${LOG_DIR}/done
+         touch ${LOG_DIR}/done
